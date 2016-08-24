8f8b9ba8d2bc625d63cafed3fad872fd3e551496
CrazyGuy108 <taylor@hansenfamily.us>
2016-08-23 19:15:45 -0700

    implement search() and initlist constructor

--- src/assembler/symboltable/symboltable.cpp
+++ src/assembler/symboltable/symboltable.cpp
@@ -1,11 +1,39 @@
 #include "symboltable.h"
 
+SymbolTable::SymbolTable(std::initializer_list<Symbol> list)
+{
+	for (auto& i : list)
+		push_back(i);
+}
+
 bool SymbolTable::resolve(char* token)
 {
-	
+	return false;	// placeholder
 }
 
 Symbol* SymbolTable::search(char* token)
 {
+	// performs a uniform binary search
 
+	size_type start{ 0 };			// start index
+	size_type end{ size() - 1 };	// end index
+	size_type middle;				// index to test
+	int comparison;					// temp value to compare strings
+	Symbol* dataptr{ nullptr };		// symbol to compare with token
+
+	while(start <= end)
+	{
+		middle = (start + end) >> 1;	// average with shift optimization
+		dataptr = &(at(middle));
+		comparison = strcmp(dataptr->name, token);
+
+		if (comparison < 0)			// data<token
+			start = middle + 1;
+		else if (comparison > 0)	// data>token
+			end = middle - 1;
+		else						// data=token
+			break;
+	}
+
+	return dataptr;
 }
