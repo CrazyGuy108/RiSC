69e359b59a49bad88b7cd2e233842ea2d2833aa7
CrazyGuy108 <taylor@hansenfamily.us>
2016-08-23 22:01:00 -0700

    start outlining how exactly to do opcodes

--- src/assembler/opcodes/opcodes.h
+++ src/assembler/opcodes/opcodes.h
@@ -1,4 +1,7 @@
 #ifndef OPCODES_H
 #define OPCODES_H
-
+// opcode class
+	// contains opcode number and inst. type(rrr, rri, ri probs enum/macro)
+// in some other file make pseudo op class
+	// gives data + conditional compilation(if needed) for how to translate
 #endif // OPCODES_H
