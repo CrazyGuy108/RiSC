3f2b9c3c20cc20f8f57a32a25f0921fc8a2c3400
CrazyGuy108 <taylor@hansenfamily.us>
2016-08-25 19:30:22 -0700

    create classes for register and immediate operands

---
+++ src/assembler/operands/operands.h
@@ -0,0 +1,38 @@
+#ifndef OPERANDS_H
+#define OPERANDS_H
+
+#include <cstdint>
+#include <array>
+#include "../symboltable/symboltable.h"
+
+template<size_t length>
+class RegTable
+	: public Table<Symbol, Symbol*>
+{
+public:
+	RegTable(Symbol* data)
+		: data{ data } {}
+};
+
+class Reg
+{
+public:
+	Reg(const char* name);
+	uint16_t value;
+};
+
+class Imm7
+{
+public:
+	Imm7(const char* name);
+	uint16_t value;
+};
+
+class Imm10
+{
+public:
+	Imm10(const char* name);
+	uint16_t value;
+};
+
+#endif // OPERANDS_H
