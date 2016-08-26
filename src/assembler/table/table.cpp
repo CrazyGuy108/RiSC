812bdfcee681e142689847982eaac35153a26456
CrazyGuy108 <taylor@hansenfamily.us>
2016-08-25 19:29:28 -0700

    create Table base class for code reusability

---
+++ src/assembler/table/table.cpp
@@ -0,0 +1,17 @@
+#include "table.h"
+
+int cmp(const char* a, const char* b)
+{
+	size_t index{ 0 };
+	do
+	{
+		if (a[index] < b[index])
+			return -1;
+		else if (a[index] > b[index])
+			return 1;
+		else
+			++index;
+	} while (a[index] && b[index]);
+
+	return 0;
+}
---
+++ src/assembler/table/table.h
@@ -0,0 +1,58 @@
+#ifndef TABLE_H
+#define TABLE_H
+
+template <typename T,	// data type
+		  typename U>	// list type, must be either T* or a class operator[] overloaded
+class Table
+{
+public:
+	typedef T data_type;
+	typedef U list_type;
+
+	virtual size_t size() = 0;
+
+	// access data by index
+	data_type& operator[](size_t index)
+	{
+		return data[index];
+	}
+
+	// access data by name
+	data_type& operator[](const char* token)
+	{
+		return data[search(token)];
+	}
+
+protected:
+	list_type data;
+
+	size_t search(const char* token)
+	{
+		// performs a binary search
+
+		size_t start{ 0 };			// start index
+		size_t end{ size() - 1 };	// end index
+		size_t middle;				// index to test
+
+		do
+		{
+			middle = (start + end) >> 1;	// average with shift optimization
+			comparison = cmp(data[middle].name, token);
+
+			if (comparison < 0)			// <
+				start = middle + 1;
+			else if (comparison > 0)	// >
+				end = middle - 1;
+			else						// =
+				return middle;
+		} while (start <= end);
+
+		return -1;
+	}
+};
+
+// cstring comparison
+// stops AFTER comparing the null terminator, not before like in strcmp
+int cmp(const char* a, const char* b);
+
+#endif // TABLE_H
