d3de9de68e0fadad397557a8917e9c7b27fc4c81
CrazyGuy108 <taylor@hansenfamily.us>
2016-08-25 19:37:11 -0700

    update include directives

--- src/assembler/assembler.h
+++ src/assembler/assembler.h
@@ -1,7 +1,9 @@
 #ifndef ASSEMBLER_H
 #define ASSEMBLER_H
 
+#include "table/table.h"
 #include "symboltable/symboltable.h"
+#include "operands/operands.h"
 #include "opcodes/opcodes.h"
 
 char* assemble(const char* program);
