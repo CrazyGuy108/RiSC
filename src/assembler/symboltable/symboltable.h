7029aa8681157efd3f746c4882c66fa132be0bfd
CrazyGuy108 <taylor@hansenfamily.us>
2016-08-25 19:31:17 -0700

    make SymbolTable inherit from Table, improving flexibility

--- src/assembler/symboltable/symboltable.h
+++ src/assembler/symboltable/symboltable.h
@@ -1,8 +1,9 @@
 #ifndef SYMBOLTABLE_H
 #define SYMBOLTABLE_H
 
-#include <cstdint>	// for uint16_t
-#include <vector>	// for std::vector
+#include <cstdint>			// for uint16_t
+#include <vector>			// for std::vector
+#include "../table/table.h"	// for Table
 
 class Symbol
 {
@@ -14,20 +15,12 @@ public:
 	uint16_t value;
 };
 
-class SymbolTable: public std::vector<Symbol>
+class SymbolTable
+	: public Table<Symbol, std::vector<Symbol>>
 {
 public:
-	// inherit vector's constructors
-	using std::vector<Symbol>::vector;
-
-	// binary search for a symbol
-	int search(const char* token);
+	// vector of symbols to build during assemble-time
+	std::vector<Symbol> data;
 };
-
-// compare 2 strings
-// compare result with 0 to get comparison result
-// i.e. -1(returned from cmp) is < because -1<0
-// strcmp("a","ab") returns 0(=) for me so I had to rewrite it here
-int cmp(const char* a, const char* b);
 
 #endif // SYMBOLTABLE_H
