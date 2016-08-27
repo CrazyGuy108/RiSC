b1fb90b93e32c68873eb19d135524ace49fa17b8
CrazyGuy108 <taylor@hansenfamily.us>
2016-08-27 11:30:52 -0700

    remove table and operand includes

--- src/assembler/assembler.h
+++ src/assembler/assembler.h
@@ -1,9 +1,7 @@
 #ifndef ASSEMBLER_H
 #define ASSEMBLER_H
 
-#include "table/table.h"
 #include "symboltable/symboltable.h"
-#include "operands/operands.h"
 #include "opcodes/opcodes.h"
 
 char* assemble(const char* program);
