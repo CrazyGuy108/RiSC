1e60a2091aa17a06130a88120666158f78ad904c
CrazyGuy108 <taylor@hansenfamily.us>
2016-08-28 14:42:50 -0700

    create class template for symbol table for flexibility

--- src/assembler/symboltable/symboltable.h
+++ src/assembler/symboltable/symboltable.h
@@ -14,35 +14,87 @@
 #define BEQ 0xC000
 #define JALR 0xE000
 
+template <typename T>
 class Symbol
 {
 public:
-	Symbol(const char* name, uint16_t value);
+	typedef const char* name_type;
+	typedef T value_type;
 
-	const char* name;
-	uint16_t value;
+	Symbol(name_type name, value_type& value)
+		: name{ name }, value{ value } {}
+
+	name_type name;
+	value_type value;
 };
 
-class SymbolTable
+template <typename T>
+class Table
 {
 public:
-	SymbolTable(std::initializer_list<Symbol> list);
+	typedef Symbol<T> symbol_type;
 
-	size_t size();
+	Table(std::initializer_list<symbol_type> data)
+		: data{ data } {}
 
-	// without bounds checking
-	Symbol& operator[](size_t index);
-	Symbol& operator[](const char* token);
+	size_t size()
+	{
+		return data.size();
+	}
 
-	// with bounds checking
-	Symbol& at(size_t index);
-	Symbol& at(const char* token);
+	symbol_type& operator[](size_t index)
+	{
+		return data[index];
+	}
+
+	symbol_type& operator[](symbol_type::name_type token)
+	{
+		return data[search(token)];
+	}
+
+	symbol_type& at(size_t index)
+	{
+		if (index < size())
+			return operator[](index);
+		else
+			abort();
+	}
+
+	symbol_type& at(symbol_type::name_type token)
+	{
+		int index{ search(token) };
+		if (index != -1)
+			return operator[](index);
+		else
+			abort();
+	}
 
 	// binary search for a Symbol's index
-	int search(const char* token);
+	int search(symbol_type::name_type token)
+	{
+		size_t start{ 0 };
+		size_t end{ size() - 1 };
+		size_t middle;
+		int comparison;
+
+		do
+		{
+			middle = (start + end) >> 1;
+			comparison = cmp(data[middle].name, token);
+
+			if (comparison < 0)			// <
+				start = middle + 1;
+			else if (comparison > 0)	// >
+				end = middle - 1;
+			else						// =
+				return middle;
+		} while (start <= end);
+
+		return -1;
+	}
 
 private:
-	std::vector<Symbol> data;
+	std::vector<symbol_type> data;
 };
 
 // string comparison that includes the null character unlike strcmp
@@ -50,6 +102,8 @@ private:
 int cmp(const char* a, const char* b);
 
 /***** move to a separate file later *****/
+typedef Table<uint16_t> SymbolTable;
+
 extern SymbolTable opcodes;
 extern SymbolTable regs;
 extern SymbolTable imms;	/***** create class/function instead of this *****/
