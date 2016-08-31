f41d0d81f085bde57d7a2628cd7fe0acbf9e5401
CrazyGuy108 <taylor@hansenfamily.us>
2016-08-30 21:10:12 -0700

    update includes in assembler.h

--- src/assembler/assembler.h
+++ src/assembler/assembler.h
@@ -4,6 +4,8 @@
 #include "table/table.h"
 #include "utilities/utilities.h"
 #include "instruction/instruction.h"
+#include "pseudo/pseudo.h"
+#include "table/table.h"
 
 char* assemble(const char* program);
 
