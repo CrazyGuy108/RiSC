138d20a8f772176f84f368b621d5bb84c69d30ab
CrazyGuy108 <taylor@hansenfamily.us>
2016-08-31 17:19:10 -0700

    add argc check for nop

--- src/assembler/pseudo/pseudo.cpp
+++ src/assembler/pseudo/pseudo.cpp
@@ -4,13 +4,17 @@ void nop(Line* thisptr, size_t argc, const char** argv)
 {
 	// translates to "add 0 0 0"
 	// no compiling step needed, so init with pre-compiled data
-
-	thisptr->instructions = new Instruction[1]
+	if (argc == 0)
 	{
-		Instruction{ { ADD, OpType::RRR }, new RRR{ 0, 0, 0 }, 3 }
-	};
+		thisptr->instructions = new Instruction[1]
+		{
+			Instruction{ { ADD, OpType::RRR }, new RRR{ 0, 0, 0 }, 3 }
+		};
 
-	thisptr->length = 1;
+		thisptr->length = 1;
+	}
+	else
+		; // error: too many operands
 }
 
 void halt(Line* thisptr, size_t argc, const char** argv)
