eff68ea0053f6e951d151efe814bfa44b501648d
CrazyGuy108 <taylor@hansenfamily.us>
2016-08-31 18:49:45 -0700

    ditch object-oriented approach for a more optimized imperative one, propagating changes in the next commits

--- src/assembler/instruction/instruction.cpp
+++ src/assembler/instruction/instruction.cpp
@@ -1,122 +1,16 @@
 #include "instruction.h"
 
-Opcode::Opcode()
-	{}
+Line::Line(uint16_t* instructions, size_t length)
+	: instructions{ instructions }, length{ length } {}
 
-Opcode::Opcode(uint16_t value, OpType type)
-	: value{ value }, type{ type } {}
-
-Operand::Operand(uint16_t value)
-	: value{ value } {}
-
-Reg::Reg(uint16_t value)
-	: Operand{ value } {}
-
-Reg::Reg(const char* name)
-	: Operand{ regs[name].value } {}
-
-Imm::Imm(uint16_t value)
-	: Operand{ value } {}
-
-Imm::Imm(const char* name)
-	: Operand{ imm(name) } {}
-
-Imm6::Imm6(const char* name)
-	: Imm{ imm(name) & 0x003f } {}
-
-Imm10::Imm10(uint16_t value)
-	: Imm{ value & 0x03ff } {}
-
-Imm10::Imm10(const char* name)
-	: Imm{ imm(name) & 0x03ff } {}
-
-RRR::RRR(const char* regA, const char* regB, const char* regC)
-	: regA{ regA }, regB{ regB }, regC{ regC } {}
-
-RRI::RRI(const char* regA, const char* regB, const char* immC)
-	: regA{ regA }, regB{ regB }, immC{ immC } {}
-
-RI::RI(const char* regA, const char* immB)
-	: regA{ regA }, immB{ immB } {}
-
-RI::RI(Reg regA, Imm10 immb)
-	: regA{ regA }, immB{ immB } {}
-
-Instruction::Instruction()
-	{}
-
-Instruction::Instruction(Opcode opcode, OperandField* operands, size_t length)
-	: opcode{ opcode }, operands{ operands }, length{ length } {}
-
-Instruction::~Instruction()
+Line compile(size_t argc, const char** argv)
 {
-	delete[] operands;
-}
-
-Line::Line(int argc, const char** argv)
-{
-	int index{ opcodes.search(argv[0]) };
-	if (index >= 0)
+	if (argc > 0)
 	{
-		const Opcode& op{ opcodes[index].value };
-
-		instructions = new Instruction[1];
-		instructions->opcode = op;
-		length = 1;
-
-		switch (op.type)
-		{
-		case OpType::RRR:
-			if (argc == 4)
-			{
-				instructions->operands = new RRR{ argv[1], argv[2], argv[3] };
-				instructions->length = 3;
-			}
-			else
-				; // error: operand amount doesn't match
-
-			break;
-
-		case OpType::RRI:
-			if (argc == 4)
-			{
-				instructions->operands = new RRI{ argv[1], argv[2], argv[3] };
-				instructions->length = 3;
-			}
-			else
-				; // error: operand amount doesn't match
-			break;
-
-		case OpType::RI:
-			if (argc == 3)
-			{
-				instructions->operands = new RI{ argv[1], argv[2] };
-				instructions->length = 2;
-			}
-			else
-				; // error: operand amount doesn't match
-			break;
-
-		default:
-			instructions->length = 0; // error: opcode type undefined (should never happen)
-		}
+		int index{ opcodes.search(argv[0]) };
+		// call appropriate function
+		// i.e. return add(argc, argv);
 	}
 	else
-	{
-		index = pseudos.search(argv[0]);
-		if (index >= 0)
-			pseudos[index].value(this, argc, argv); // evaluate as pseudo op by calling the contained function
-		else
-			; // error: undefined opcode
-	}
-}
-
-Line::~Line()
-{
-	delete instructions;
-}
-
-uint16_t* Line::compile()
-{
-
+		return Line{ nullptr, 0 }; // disregard as a blank line
 }
--- src/assembler/instruction/instruction.h
+++ src/assembler/instruction/instruction.h
@@ -2,152 +2,25 @@
 #define INSTRUCTION_H
 
 // prototypes
-enum class OpType;
-class Opcode;
-class Operand;
-class Reg;
-class Imm;
-class OperandField;
-class RRR;
-class RRI;
-class RI;
-class Instruction;
 class Line;
 
 #include <cstdint>
 #include "../table/table.h"
 #include "../utilities/utilities.h"
 
-// opcode enum and class
-
-enum class OpType
-{
-	RRR,
-	RRI,
-	RI
-};
-
-class Opcode
-{
-public:
-	Opcode();
-	Opcode(uint16_t value, OpType type);
-
-	uint16_t value;
-	OpType type;
-};
-
-// operand class and inherited forms
-
-class Operand
-{
-public:
-	Operand(uint16_t value);
-
-	uint16_t value;
-};
-
-class Reg
-	: public Operand
-{
-public:
-	Reg(uint16_t value);
-	Reg(const char* name);
-};
-
-class Imm
-	: public Operand
-{
-public:
-	Imm(uint16_t value);
-	Imm(const char* name);
-};
-
-class Imm6
-	: public Imm
-{
-public:
-	Imm6(const char* name);
-};
-
-class Imm10
-	: public Imm
-{
-public:
-	Imm10(uint16_t value);
-	Imm10(const char* name);
-};
-
-// operand field class and inherited forms
-
-class OperandField
-{
-
-};
-
-class RRR
-	: public OperandField
-{
-public:
-	RRR(const char* regA, const char* regB, const char* regC);
-
-	Reg regA;
-	Reg regB;
-	Reg regC;
-};
-
-class RRI
-	: public OperandField
-{
-public:
-	RRI(const char* regA, const char* regB, const char* immC);
-
-	Reg regA;
-	Reg regB;
-	Imm6 immC;
-};
-
-class RI
-	: public OperandField
-{
-public:
-	RI(const char* regA, const char* immB);
-	RI(Reg regA, Imm10 immb);
-
-	Reg regA;
-	Imm10 immB;
-};
-
-// semi-compiled instruction class
-
-class Instruction
-{
-public:
-	Instruction();
-	Instruction(Opcode opcode, OperandField* operands, size_t length);
-	~Instruction();
-
-	Opcode opcode;
-	OperandField* operands;
-	size_t length;
-};
-
-// instruction container class
-
 class Line
 {
 public:
-	Line(int argc, const char** argv);
-	~Line();
+	Line(uint16_t* instructions, size_t length);
 
-	// uses all the constructed object heiarchy to compile into a series of bytecode
-	uint16_t* compile();
-
-	// array of Instructions to translate into
-	Instruction* instructions;
+	// array of compiled instructions
+	uint16_t* instructions;
 
 	// keeps track of how many Instructions
 	size_t length;
 };
+
+// takes in a parsed line of code to produce a series of bytecode instructions
+Line compile(size_t argc, const char** argv);
 
 #endif // INSTRUCTION_H
