a18cc48c237786bbe9fc0b9b01e258eef9fb03df
CrazyGuy108 <taylor@hansenfamily.us>
2016-08-31 17:17:57 -0700

    implement nop pseudo instruction

--- src/assembler/pseudo/pseudo.cpp
+++ src/assembler/pseudo/pseudo.cpp
@@ -2,7 +2,15 @@
 
 void nop(Line* thisptr, size_t argc, const char** argv)
 {
+	// translates to "add 0 0 0"
+	// no compiling step needed, so init with pre-compiled data
 
+	thisptr->instructions = new Instruction[1]
+	{
+		Instruction{ { ADD, OpType::RRR }, new RRR{ 0, 0, 0 }, 3 }
+	};
+
+	thisptr->length = 1;
 }
 
 void halt(Line* thisptr, size_t argc, const char** argv)
