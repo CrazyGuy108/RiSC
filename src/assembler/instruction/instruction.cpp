bd0bbb93f046fc5fb9370ccba41c02323a834d42
CrazyGuy108 <taylor@hansenfamily.us>
2016-09-01 16:37:09 -0700

    reflect changes in utilities with merging of pseudo op and opcode

--- src/assembler/instruction/instruction.cpp
+++ src/assembler/instruction/instruction.cpp
@@ -8,21 +8,13 @@ Line compile(size_t argc, const char** argv)
 {
 	if (argc > 0)
 	{
-		int index{ opcodes.search(argv[0]) };
+		int index{ ops.search(argv[0]) };
 		if (index != -1)
 		{
-			return opcodes[index].value(argc, argv);
+			return ops[index].value(argc, argv);
 		}
 		else
-		{
-			index = pseudos.search(argv[0]);
-			if (index != -1)
-			{
-				return pseudos[index].value(argc, argv);
-			}
-			else
-				; // error: undefined opcode
-		}
+			; // error: undefined opcode
 	}
 	else
 		return Line{ nullptr, 0 }; // disregard as a blank line
