27c5e4a58358d794c377ad615e115cc2bc02f643
CrazyGuy108 <taylor@hansenfamily.us>
2016-08-31 21:14:03 -0700

    add opcode include and alphabetically order them in assembler.h

--- src/assembler/assembler.h
+++ src/assembler/assembler.h
@@ -1,11 +1,11 @@
 #ifndef ASSEMBLER_H
 #define ASSEMBLER_H
 
-#include "table/table.h"
-#include "utilities/utilities.h"
 #include "instruction/instruction.h"
+#include "opcode/opcode.h"
 #include "pseudo/pseudo.h"
 #include "table/table.h"
+#include "utilities/utilities.h"
 
 char* assemble(const char* program);
 
