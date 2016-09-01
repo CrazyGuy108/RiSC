cbcfa5720612f1f7360a651796fda8fd1d30a809
CrazyGuy108 <taylor@hansenfamily.us>
2016-08-31 19:51:01 -0700

    remove thisptr parameter from pseudo op functions and function ptr typedef

--- src/assembler/pseudo/pseudo.h
+++ src/assembler/pseudo/pseudo.h
@@ -6,17 +6,17 @@ class Line;
 // function pointer typedef for the functions below
 typedef Line(*Pseudo)(size_t argc, const char** argv);
 
-void nop(size_t argc, const char** argv);
+Line nop(size_t argc, const char** argv);
 
-void halt(size_t argc, const char** argv);
+Line halt(size_t argc, const char** argv);
 
-void lli(size_t argc, const char** argv);
+Line lli(size_t argc, const char** argv);
 
-void movi(size_t argc, const char** argv);
+Line movi(size_t argc, const char** argv);
 
-void fill(size_t argc, const char** argv);
+Line fill(size_t argc, const char** argv);
 
-void space(size_t argc, const char** argv);
+Line space(size_t argc, const char** argv);
 
 #include "../instruction/instruction.h"
 
