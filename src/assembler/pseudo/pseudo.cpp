25da9b8ef5da90602b7e0f5dbb9f3de127f82c0e
CrazyGuy108 <taylor@hansenfamily.us>
2016-08-31 17:33:14 -0700

    implement movi pseudo-op

--- src/assembler/pseudo/pseudo.cpp
+++ src/assembler/pseudo/pseudo.cpp
@@ -40,12 +40,45 @@ void lli(Line* thisptr, size_t argc, const char** argv)
 
 void movi(Line* thisptr, size_t argc, const char** argv)
 {
+	if (argc == 3)
+	{
+		uint16_t immed{ imm(argv[2]) };
+		if (immed & 0xffc0 == immed) // use only lui
+		{
+			thisptr->instructions = new Instruction[1]
+			{
+				Instruction{ { LUI, OpType::RI }, new RI{ argv[1], argv[2] }, 2 }
+			};
 
+			thisptr->length = 1;
+		}
+		else if(immed & 0x003f == immed) // use only lli
+		{
+			thisptr->instructions = new Instruction[1]
+			{
+				Instruction{ { ADDI, OpType::RRI }, new RRI{ argv[1], argv[1], argv[2] }, 3 }
+			};
+
+			thisptr->length = 1;
+		}
+		else // use both lui and lli
+		{
+			thisptr->instructions = new Instruction[2]
+			{
+				Instruction{ { LUI, OpType::RI }, new RI{ argv[1], argv[2] }, 2 }, // lui
+				Instruction{ { ADDI, OpType::RRI }, new RRI{ argv[1], argv[1], argv[2] }, 3 } // lli
+			};
+
+			thisptr->length = 2;
+		}
+	}
+	else
+		; // error: operand count does not match
 }
 
 void fill(Line* thisptr, size_t argc, const char** argv)
 {
-
+	// immediate replaces the entire instruction
 }
 
 void space(Line* thisptr, size_t argc, const char** argv)
