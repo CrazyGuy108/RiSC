e3a7845c906683a4c4ad72b26a2bc178a110e5a4
CrazyGuy108 <taylor@hansenfamily.us>
2016-08-31 19:16:23 -0700

    remove pseudo op function bodies for now, since major change is happening; will rewrite soon

--- src/assembler/pseudo/pseudo.cpp
+++ src/assembler/pseudo/pseudo.cpp
@@ -3,18 +3,12 @@
 void nop(Line* thisptr, size_t argc, const char** argv)
 {
 	// translates from "nop" to "add 0 0 0"
-	// no compiling step needed, so init with pre-compiled data
 	if (argc == 1)
 	{
-		thisptr->instructions = new Instruction[1]
-		{
-			Instruction{ { ADD, OpType::RRR }, new RRR{ "0", "0", "0" }, 3 }
-		};
-
-		thisptr->length = 1;
+		
 	}
 	else
-		; // error: too many operands
+		; // error: operand count does not match
 }
 
 void halt(Line* thisptr, size_t argc, const char** argv)
@@ -27,12 +21,7 @@ void lli(Line* thisptr, size_t argc, const char** argv)
 	// translates from "lli rA, imm6" to "addi rA, rA, imm6"
 	if (argc == 3)
 	{
-		thisptr->instructions = new Instruction[1]
-		{
-			Instruction{ { ADDI, OpType::RRI }, new RRI{ argv[1], argv[1], argv[2] }, 3 }
-		};
-
-		thisptr->length = 1;
+		
 	}
 	else
 		; // error: operand count does not match
@@ -45,31 +34,15 @@ void movi(Line* thisptr, size_t argc, const char** argv)
 		uint16_t immed{ imm(argv[2]) };
 		if (immed & 0xffc0 == immed) // use only lui
 		{
-			thisptr->instructions = new Instruction[1]
-			{
-				Instruction{ { LUI, OpType::RI }, new RI{ argv[1], argv[2] }, 2 }
-			};
-
-			thisptr->length = 1;
+			
 		}
 		else if(immed & 0x003f == immed) // use only lli
 		{
-			thisptr->instructions = new Instruction[1]
-			{
-				Instruction{ { ADDI, OpType::RRI }, new RRI{ argv[1], argv[1], argv[2] }, 3 }
-			};
-
-			thisptr->length = 1;
+			
 		}
 		else // use both lui and lli
 		{
-			thisptr->instructions = new Instruction[2]
-			{
-				Instruction{ { LUI, OpType::RI }, new RI{ argv[1], argv[2] }, 2 }, // lui
-				Instruction{ { ADDI, OpType::RRI }, new RRI{ argv[1], argv[1], argv[2] }, 3 } // lli
-			};
-
-			thisptr->length = 2;
+			
 		}
 	}
 	else
@@ -81,11 +54,7 @@ void fill(Line* thisptr, size_t argc, const char** argv)
 	// immediate replaces the entire instruction
 	if (argc == 2)
 	{
-		uint16_t immed{ imm(argv[1]) };
-		thisptr->instructions = new Instruction[1]
-		{
-			Instruction{ { immed, OpType::RI }, new RI{ Reg{ immed }, Imm10{ immed } }, 2 }
-		};
+		
 	}
 	else
 		; // error: operand count does not match
