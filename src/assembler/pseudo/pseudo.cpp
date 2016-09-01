288a803d3915c4e7858470adf71dcf8b9a2fef6e
CrazyGuy108 <taylor@hansenfamily.us>
2016-08-31 17:23:13 -0700

    implement lli pseudo-op

--- src/assembler/pseudo/pseudo.cpp
+++ src/assembler/pseudo/pseudo.cpp
@@ -4,7 +4,7 @@ void nop(Line* thisptr, size_t argc, const char** argv)
 {
 	// translates from "nop" to "add 0 0 0"
 	// no compiling step needed, so init with pre-compiled data
-	if (argc == 0)
+	if (argc == 1)
 	{
 		thisptr->instructions = new Instruction[1]
 		{
@@ -24,7 +24,18 @@ void halt(Line* thisptr, size_t argc, const char** argv)
 
 void lli(Line* thisptr, size_t argc, const char** argv)
 {
+	// translates from "lli rA, imm6" to "addi rA, rA, imm6"
+	if (argc == 3)
+	{
+		thisptr->instructions = new Instruction[1]
+		{
+			Instruction{ { ADDI, OpType::RRI }, new RRI{ argv[1], argv[1], argv[2] }, 3 }
+		};
 
+		thisptr->length = 1;
+	}
+	else
+		; // error: operand count does not match
 }
 
 void movi(Line* thisptr, size_t argc, const char** argv)
