cc8dd695170e5d5ceb454892aacdc1128eccd1e1
CrazyGuy108 <taylor@hansenfamily.us>
2016-08-31 19:53:13 -0700

    add reminder and implement compile function

--- src/assembler/instruction/instruction.cpp
+++ src/assembler/instruction/instruction.cpp
@@ -3,13 +3,26 @@
 Line::Line(uint16_t* instructions, size_t length)
 	: instructions{ instructions }, length{ length } {}
 
+/***** move to utilities and rename instruction to line? *****/
 Line compile(size_t argc, const char** argv)
 {
 	if (argc > 0)
 	{
 		int index{ opcodes.search(argv[0]) };
-		// call appropriate function
-		// i.e. return add(argc, argv);
+		if (index != -1)
+		{
+			return opcodes[index].value(argc, argv);
+		}
+		else
+		{
+			index = pseudos.search(argv[0]);
+			if (index != -1)
+			{
+				return pseudos[index].value(argc, argv);
+			}
+			else
+				; // error: undefined opcode
+		}
 	}
 	else
 		return Line{ nullptr, 0 }; // disregard as a blank line
