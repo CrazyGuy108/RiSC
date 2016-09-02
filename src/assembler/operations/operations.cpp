e29a028e1e80719b6cb10a355995c521e8b99050
CrazyGuy108 <taylor@hansenfamily.us>
2016-09-01 18:38:43 -0700

    implement add, start thinking of a better way to compile the bytecode

--- src/assembler/operations/operations.cpp
+++ src/assembler/operations/operations.cpp
@@ -2,7 +2,25 @@
 
 Line add(size_t argc, const char** argv)
 {
+	if (argc == 4)
+	{
+		int operands[3]
+		{
+			regs.search(argv[1]),
+			regs.search(argv[2]),
+			regs.search(argv[3])
+		};
 
+		for (uint8_t i{ 0 }; i < 3; ++i)
+		{
+			if (operands[i] < 0)
+				; // error: undefined operand
+		}
+
+		return Line{ new uint16_t[1]{ ADD | (regs[operands[0]].value << 13) | (regs[operands[0]].value << 10) | (regs[operands[0]].value) }, 1 };
+	}
+	else
+		; // error: operand count does not match
 }
 
 Line addi(size_t argc, const char** argv)
