e100cb3bd2a362e7e1f50d2c58f68ae33f0ce372
CrazyGuy108 <taylor@hansenfamily.us>
2016-09-04 09:11:45 -0700

    start string parsing code

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,16 @@
+#include <fstream>
 #include "assembler/assembler.h"
 
 int main(int argc, char** argv)
 {
+	if (argc == 2)
+	{
+		std::ifstream source{ argv[2] };
+		
+		source.close();
+	}
+	else
+		; // error: no file
+	
 	return 0;
 }
