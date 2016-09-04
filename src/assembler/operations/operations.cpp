f43b855756025ee87305294ebfe84050885527c0
CrazyGuy108 <taylor@hansenfamily.us>
2016-09-03 20:39:46 -0700

    implement sw

--- src/assembler/operations/operations.cpp
+++ src/assembler/operations/operations.cpp
@@ -53,7 +53,18 @@ Line lui(size_t argc, const char** argv)
 
 Line sw(size_t argc, const char** argv)
 {
+	if (argc == 4)
+	{
+		uint16_t immed{ imm(argv[3]) };
 
+		/***** improve immed check *****/
+		if (immed & IMM7_MASK == immed || immed & IMM7_NEG_MASK == IMM7_NEG_MASK)
+			return Line{ new RRI[1]{ RRI{ SW, argv[1], argv[2], immed } }, 1 };
+		else
+			; // either pseudo op or error: immediate too big
+	}
+	else
+		; // error: operand count does not match
 }
 
 Line lw(size_t argc, const char** argv)
