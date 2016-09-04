ec611f6173cadbd4f82fb74926cfe09bce69bc9d
CrazyGuy108 <taylor@hansenfamily.us>
2016-09-03 18:10:38 -0700

    create nand definition

--- src/assembler/operations/operations.cpp
+++ src/assembler/operations/operations.cpp
@@ -47,7 +47,25 @@ Line addi(size_t argc, const char** argv)
 
 Line nand(size_t argc, const char** argv)
 {
+	if (argc == 4)
+	{
+		int operands[3]
+		{
+			regs.search(argv[1]),
+			regs.search(argv[2]),
+			regs.search(argv[3])
+		};
 
+		for (uint8_t i{ 0 }; i < 3; ++i)
+		{
+			if (operands[i] < 0)
+				; // error: undefined operand
+		}
+
+		return Line{ new RRR[1]{ RRR{ NAND, regs[operands[0]], regs[operands[1]], regs[operands[2]] } }, 1 };
+	}
+	else
+		; // error: operand count does not match
 }
 
 Line lui(size_t argc, const char** argv)
