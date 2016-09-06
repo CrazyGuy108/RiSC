99b3b02c34b650870cd156075d4542b2b1fff550
CrazyGuy108 <taylor@hansenfamily.us>
2016-09-05 22:27:18 -0700

    add length check for file input

--- src/assembler/assembler.cpp
+++ src/assembler/assembler.cpp
@@ -4,5 +4,10 @@ void assemble(int argc, char** argv)
 {
 	std::ifstream infile{ argv[1] };
 
+	// get file length
+	infile.seekg(0, infile.end);
+	std::streamoff length = infile.tellg();
+	infile.seekg(0, infile.beg);
+
 	infile.close();
 }
