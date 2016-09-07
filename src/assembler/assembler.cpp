233f1209137a4b10c8e630757bff36f35fd12ae0
CrazyGuy108 <taylor@hansenfamily.us>
2016-09-06 21:56:35 -0700

    add write file to assembler

--- src/assembler/assembler.cpp
+++ src/assembler/assembler.cpp
@@ -2,16 +2,33 @@
 
 void assemble(int argc, char** argv)
 {
-	std::ifstream infile{ argv[1] };
+	if (argc == 3)
+	{
+		std::ifstream infile{ argv[1] };
 
-	// get file length
-	infile.seekg(0, infile.end);
-	std::streamsize length = infile.tellg();
-	infile.seekg(0, infile.beg);
+		// get file length
+		infile.seekg(0, infile.end);
+		std::streamsize length = infile.tellg();
+		infile.seekg(0, infile.beg);
 
-	// put into char array
-	char* contents = new char[length];
-	infile.read(contents, length);
+		// put into char array
+		char* contents = new char[length];
+		infile.read(contents, length);
 
-	infile.close();
+		// no need to read from it anymore
+		infile.close();
+
+		// split into words and ignore comments
+
+		// build symbol table
+		
+		// compile into bytecode
+		
+		// write to file
+		std::ofstream outfile{ argv[2] };
+
+		outfile.close();
+	}
+	else
+		; // error: no dest file specified
 }
