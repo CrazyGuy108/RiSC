0d52eca39c39256c0ec9f5c13eabaa9c0840d26a
CrazyGuy108 <taylor@hansenfamily.us>
2016-09-24 20:51:42 -0700

    replace constructors with functions
    reflects changes in instruciton.h

--- src/assembler/instruction/instruction.cpp
+++ src/assembler/instruction/instruction.cpp
@@ -1,15 +1,11 @@
 #include "instruction.h"
 
-Instruction::Instruction()
-	{}
+line_t rrr(uint16_t opcode, uint16_t regA, uint16_t regB, uint16_t regC)
+{
+	return line_t{ opcode | (regA << A_SHIFT) | (regB << B_SHIFT) | regC };
+}
 
-Instruction::Instruction(uint16_t value)
-	: value{ value } {}
-
-RRR::RRR(uint16_t opcode, uint16_t regA, uint16_t regB, uint16_t regC)
-	: Instruction{ opcode | (regA << A_SHIFT) | (regB << B_SHIFT) | regC } {}
-
-RRR::RRR(uint16_t opcode, const char* regA, const char* regB, const char* regC)
+line_t rrr(uint16_t opcode, const char* regA, const char* regB, const char* regC)
 {
 	int operands[3]
 	{
@@ -24,13 +20,15 @@ RRR::RRR(uint16_t opcode, const char* regA, const char* regB, const char* regC)
 			; // error: undefined operand
 	}
 
-	*this = RRR{ opcode, regs[operands[0]], regs[operands[1]], regs[operands[2]] };
+	return rrr(opcode, regs[operands[0]], regs[operands[1]], regs[operands[2]]);
 }
 
-RRI::RRI(uint16_t opcode, uint16_t regA, uint16_t regB, uint16_t immC)
-	: Instruction{opcode | (regA << A_SHIFT) | (regB << B_SHIFT) | (immC & IMM7_MASK) } {}
+line_t rri(uint16_t opcode, uint16_t regA, uint16_t regB, uint16_t immC)
+{
+	return line_t{ opcode | (regA << A_SHIFT) | (regB << B_SHIFT) | (immC & IMM7_MASK) };
+}
 
-RRI::RRI(uint16_t opcode, const char* regA, const char* regB, uint16_t immC)
+line_t rri(uint16_t opcode, const char* regA, const char* regB, uint16_t immC)
 {
 	int operands[2]
 	{
@@ -44,36 +42,25 @@ RRI::RRI(uint16_t opcode, const char* regA, const char* regB, uint16_t immC)
 			; // error: undefined operand
 	}
 
-	*this = RRI{ opcode, regs[operands[0]], regs[operands[1]], immC };
+	return rri(opcode, regs[operands[0]], regs[operands[1]], immC);
 }
 
-RI::RI(uint16_t opcode, uint16_t regA, uint16_t immB)
-	: Instruction{ opcode | (regA << A_SHIFT) + (immB & IMM10_MASK) } {}
+line_t ri(uint16_t opcode, uint16_t regA, uint16_t immB)
+{
+	return line_t{ opcode | (regA << A_SHIFT) + (immB & IMM10_MASK) };
+}
 
-RI::RI(uint16_t opcode, const char* regA, uint16_t immB)
+line_t ri(uint16_t opcode, const char* regA, uint16_t immB)
 {
 	int operand{ regs.search(regA) };
 
 	if (operand >= 0)
-		*this = RI{ opcode, regs[operand], immB };
+		return ri(opcode, regs[operand], immB);
 	else
 		; // error: undefined operand
 }
 
-Line::Line()
-	{}
-
-Line::Line(Instruction* instructions, size_t length)
-	: instructions{ instructions }, length{ length } {}
-
-Line::~Line()
-{
-	if (instructions != nullptr)
-		delete[] instructions;
-}
-
-/***** move to utilities and rename instruction to line? *****/
-Line compile(size_t argc, const char** argv, uint16_t line)
+line_t compile(size_t argc, const char** argv, uint16_t line)
 {
 	if (argc > 0)
 	{
@@ -86,5 +73,5 @@ Line compile(size_t argc, const char** argv, uint16_t line)
 			; // error: undefined opcode
 	}
 	else
-		return Line{ nullptr, 0 }; // disregard as a blank line
+		return line_t{}; // disregard as a blank line
 }
