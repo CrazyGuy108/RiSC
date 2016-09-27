e80df25fa6ac93a73c0e1d18f72f6456310f6556
CrazyGuy108 <taylor@hansenfamily.us>
2016-09-26 19:17:50 -0700

    remove const identifier for argv
    prevents mixing char** with const char**

--- src/assembler/instruction/instruction.h
+++ src/assembler/instruction/instruction.h
@@ -14,7 +14,7 @@ line_t ri(uint16_t opcode, const char* regA, uint16_t immB);
 
 
 // takes in a parsed line of code to produce a series of bytecode instructions
-line_t compile(size_t argc, const char** argv, uint16_t line);
+line_t compile(size_t argc, char** argv, uint16_t line);
 
 
 #endif // INSTRUCTION_H
