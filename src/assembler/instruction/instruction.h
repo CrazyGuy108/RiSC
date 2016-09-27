cc24130fb61f4b44218bf456fad94c6341faa2b5
CrazyGuy108 <taylor@hansenfamily.us>
2016-09-26 19:50:42 -0700

    reflect function signature change
    forgot to change the declaration when the definition was changed in instruction.cpp

--- src/assembler/instruction/instruction.h
+++ src/assembler/instruction/instruction.h
@@ -3,18 +3,18 @@
 
 #include "../utilities/utilities.h"
 
-line_t rrr(uint16_t opcode, uint16_t regA, uint16_t regB, uint16_t regC);
-line_t rrr(uint16_t opcode, const char* regA, const char* regB, const char* regC);
+line_t rrr(inst_t opcode, inst_t regA, inst_t regB, inst_t regC);
+line_t rrr(inst_t opcode, const char* regA, const char* regB, const char* regC);
 
-line_t rri(uint16_t opcode, uint16_t regA, uint16_t regB, uint16_t immC);
-line_t rri(uint16_t opcode, const char* regA, const char* regB, uint16_t immC);
+line_t rri(inst_t opcode, inst_t regA, inst_t regB, inst_t immC);
+line_t rri(inst_t opcode, const char* regA, const char* regB, inst_t immC);
 
-line_t ri(uint16_t opcode, uint16_t regA, uint16_t immB);
-line_t ri(uint16_t opcode, const char* regA, uint16_t immB);
+line_t ri(inst_t opcode, inst_t regA, inst_t immB);
+line_t ri(inst_t opcode, const char* regA, inst_t immB);
 
 
 // takes in a parsed line of code to produce a series of bytecode instructions
-line_t compile(size_t argc, char** argv, uint16_t line);
+line_t compile(size_t argc, char** argv, inst_t line);
 
 
 #endif // INSTRUCTION_H
