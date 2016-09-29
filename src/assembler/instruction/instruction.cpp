93fe8d1914942b0772fc220c9340bba94a6f7cab
CrazyGuy108 <taylor@hansenfamily.us>
2016-09-28 19:06:30 -0700

    use new reg() function in rrr/rri/ri functions
    reducing code duplication one commit at a time

--- src/assembler/instruction/instruction.cpp
+++ src/assembler/instruction/instruction.cpp
@@ -7,20 +7,7 @@ inst_t rrr(inst_t opcode, inst_t regA, inst_t regB, inst_t regC)
 
 inst_t rrr(inst_t opcode, const char* regA, const char* regB, const char* regC)
 {
-	int operands[3]
-	{
-		regs.search(regA),
-		regs.search(regB),
-		regs.search(regC)
-	};
-
-	for (uint8_t i{ 0 }; i < 3; ++i)
-	{
-		if (operands[i] < 0)
-			; // error: undefined operand
-	}
-
-	return rrr(opcode, regs[operands[0]], regs[operands[1]], regs[operands[2]]);
+	return rrr(opcode, reg(regA), reg(regB), reg(regC));
 }
 
 inst_t rri(inst_t opcode, inst_t regA, inst_t regB, inst_t immC)
@@ -30,19 +17,7 @@ inst_t rri(inst_t opcode, inst_t regA, inst_t regB, inst_t immC)
 
 inst_t rri(inst_t opcode, const char* regA, const char* regB, inst_t immC)
 {
-	int operands[2]
-	{
-		regs.search(regA),
-		regs.search(regB)
-	};
-
-	for (uint8_t i{ 0 }; i < 2; ++i)
-	{
-		if (operands[i] < 0)
-			; // error: undefined operand
-	}
-
-	return rri(opcode, regs[operands[0]], regs[operands[1]], immC);
+	return rri(opcode, reg(regA), reg(regB), immC);
 }
 
 inst_t ri(inst_t opcode, inst_t regA, inst_t immB)
@@ -52,12 +27,7 @@ inst_t ri(inst_t opcode, inst_t regA, inst_t immB)
 
 inst_t ri(inst_t opcode, const char* regA, inst_t immB)
 {
-	int operand{ regs.search(regA) };
-
-	if (operand >= 0)
-		return ri(opcode, regs[operand], immB);
-	else
-		; // error: undefined operand
+	return ri(opcode, reg(regA), immB);
 }
 
 line_t compile(size_t argc, const char** argv, uint16_t line)
