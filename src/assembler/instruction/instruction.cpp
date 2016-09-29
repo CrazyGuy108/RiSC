eb9fa112cc9c7b4d3ade164fb1a11b8e9ce465f6
CrazyGuy108 <taylor@hansenfamily.us>
2016-09-28 19:53:10 -0700

    remove instruction overloads
    better to call reg() directly in the opcode function

--- src/assembler/instruction/instruction.cpp
+++ src/assembler/instruction/instruction.cpp
@@ -5,27 +5,12 @@ inst_t rrr(inst_t opcode, inst_t regA, inst_t regB, inst_t regC)
 	return opcode | (regA << a_shift) | (regB << b_shift) | regC;
 }
 
-inst_t rrr(inst_t opcode, const char* regA, const char* regB, const char* regC)
-{
-	return rrr(opcode, reg(regA), reg(regB), reg(regC));
-}
-
 inst_t rri(inst_t opcode, inst_t regA, inst_t regB, inst_t immC)
 {
 	return opcode | (regA << a_shift) | (regB << b_shift) | (immC & imm7_mask);
 }
 
-inst_t rri(inst_t opcode, const char* regA, const char* regB, inst_t immC)
-{
-	return rri(opcode, reg(regA), reg(regB), immC);
-}
-
 inst_t ri(inst_t opcode, inst_t regA, inst_t immB)
 {
 	return opcode | (regA << a_shift) + (immB & imm10_mask);
-}
-
-inst_t ri(inst_t opcode, const char* regA, inst_t immB)
-{
-	return ri(opcode, reg(regA), immB);
 }
--- src/assembler/instruction/instruction.h
+++ src/assembler/instruction/instruction.h
@@ -5,12 +5,7 @@
 #include "../parse/parse.h"         // for reg()
 
 inst_t rrr(inst_t opcode, inst_t regA, inst_t regB, inst_t regC);
-inst_t rrr(inst_t opcode, const char* regA, const char* regB, const char* regC);
-
 inst_t rri(inst_t opcode, inst_t regA, inst_t regB, inst_t immC);
-inst_t rri(inst_t opcode, const char* regA, const char* regB, inst_t immC);
-
 inst_t ri(inst_t opcode, inst_t regA, inst_t immB);
-inst_t ri(inst_t opcode, const char* regA, inst_t immB);
 
 #endif // INSTRUCTION_H
