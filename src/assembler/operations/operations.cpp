f8907f2e3da335329466c856777aafd6c7a299e6
CrazyGuy108 <taylor@hansenfamily.us>
2016-09-28 17:35:29 -0700

    use line_t constructor in operation functions
    applies the change in the functions in instruction.h/instruction.cpp

--- src/assembler/operations/operations.cpp
+++ src/assembler/operations/operations.cpp
@@ -4,7 +4,7 @@ OP(add)
 {
 	if (argc == 4)
 	{
-		return rrr(ADD, argv[1], argv[2], argv[3]);
+		return line_t{ rrr(ADD, argv[1], argv[2], argv[3]) };
 	}
 	else
 		; // error: operand count does not match
@@ -13,7 +13,7 @@ OP(add)
 OP(addi)
 {
 	if (argc == 4)
-		return rri(ADDI, argv[1], argv[2], imm(argv[3], 0, imm7_mask));
+		return line_t{ rri(ADDI, argv[1], argv[2], imm(argv[3], 0, imm7_mask)) };
 	else
 		; // error: operand count does not match
 }
@@ -22,7 +22,7 @@ OP(nand)
 {
 	if (argc == 4)
 	{
-		return rrr(NAND, argv[1], argv[2], argv[3]);
+		return line_t{ rrr(NAND, argv[1], argv[2], argv[3]) };
 	}
 	else
 		; // error: operand count does not match
@@ -31,7 +31,7 @@ OP(nand)
 OP(lui)
 {
 	if (argc == 3)
-		return ri(LUI, argv[1], imm(argv[2], 0, imm10_mask));
+		return line_t{ ri(LUI, argv[1], imm(argv[2], 0, imm10_mask)) };
 	else
 		; // error: operand count does not match
 }
@@ -39,7 +39,7 @@ OP(lui)
 OP(sw)
 {
 	if (argc == 4)
-		return rri(SW, argv[1], argv[2], imm(argv[3], 0, imm7_mask));
+		return line_t{ rri(SW, argv[1], argv[2], imm(argv[3], 0, imm7_mask)) };
 	else
 		; // error: operand count does not match
 }
@@ -48,7 +48,7 @@ OP(lw)
 {
 	if (argc == 4)
 	{
-		return rri(LW, argv[1], argv[2], imm(argv[3], 0, imm7_mask));
+		return line_t{ rri(LW, argv[1], argv[2], imm(argv[3], 0, imm7_mask)) };
 	}
 	else
 		; // error: operand count does not match
@@ -57,7 +57,7 @@ OP(lw)
 OP(beq)
 {
 	if (argc == 4)
-		return rri(BEQ, argv[1], argv[2], imm(argv[3], line, imm7_mask));
+		return line_t{ (BEQ, argv[1], argv[2], imm(argv[3], line, imm7_mask)) };
 	else
 		; // error: operand count does not match
 }
@@ -65,7 +65,7 @@ OP(beq)
 OP(jalr)
 {
 	if (argc == 4)
-		return rri(JALR, argv[1], argv[2], imm(argv[3], 0, imm7_mask));
+		return line_t{ rri(JALR, argv[1], argv[2], imm(argv[3], 0, imm7_mask)) };
 	else
 		; // error: operand count does not match
 }
