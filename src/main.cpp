2782dc738ee42a93d7a1ea0b7c0579453ed4bdd9
CrazyGuy108 <taylor@hansenfamily.us>
2016-09-30 15:30:23 -0700

    improve test in main()
    planning on removing the operator[] overload that uses a numerical index

--- src/main.cpp
+++ src/main.cpp
@@ -16,25 +16,7 @@ int main(int argc, char** argv)
 	argv[1] = "test.asm";
 
 	if (argc >= 2)
-	{
-		// get file extension
-		/*test
-		const char* extension{ strchr(argv[1], '/') };
-
-		if (extension == nullptr)
-			extension = strchr(argv[1], '.');
-		else
-			extension = strchr(extension, '.');
-
-		// verify extension
-
-		int index{ extensions.search(extension) };
-		*/ int index{ 0 };
-		if (index >= 0)
-			extensions[index](argc, argv);
-		else
-			; // error: invalid extension
-	}
+		extensions[".asm"](argc, argv);
 	else
 		; // error: no file
 
