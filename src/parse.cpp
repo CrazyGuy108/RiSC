f47264f67158b2cdf6be7c68abc63ea07be0ee85
CrazyGuy108 <taylor@hansenfamily.us>
2016-10-02 16:38:15 -0700

    remove label resolving in compile()
    not needed since the preprocessor will be doing all that

--- src/parse.cpp
+++ src/parse.cpp
@@ -44,21 +44,7 @@ line_t compile(size_t argc, char** argv, uint16_t line)
 	};
 
 	if (argc > 0)
-	{
-		size_t len{ 0 };
-		while (argv[0][len] != '\0') // get length of string
-			++len;
-
-		if (argv[0][len - 1] == ':') // words ending in a colon are considered labels
-		{                            // technically, the label can contain a colon in this method
-			argv[0][len - 1] = '\0';
-			symbols.insert(argv[0], line);
-			argv = &argv[1]; // ignore label
-			--argc;
-		}
-
 		return ops[argv[0]].getFunc()(argc, (const char**)argv, line);
-	}
 	else
 		; // error: undetected blank line (could be a label on a blank line)
 }
