ed1d1366ce0afb4576aef6e96bca10852dcc444b
CrazyGuy108 <taylor@hansenfamily.us>
2016-10-03 16:36:14 -0700

    add operations include
    while redundant, header dependencies can change in the future so it's best to still have it there in case something goes wrong

--- include/assembler.h
+++ include/assembler.h
@@ -1,10 +1,11 @@
 #ifndef ASSEMBLER_H
 #define ASSEMBLER_H
 
-#include <fstream>  // for ifstream and ostream
-#include <iostream> // for cout
-#include <vector>   // for vector
-#include "parse.h"  // for compile()
+#include <fstream>      // for ifstream and ostream
+#include <iostream>     // for cout
+#include <vector>       // for vector
+#include "operations.h" // for Opcode
+#include "parse.h"      // for compile()
 
 void preprocess(std::vector<std::vector<char*>>& words, std::vector<OP((*))>& opcodes, char* iterator);
 
