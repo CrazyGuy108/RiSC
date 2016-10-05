e3d9f8a90790b0d7671e53eb95a9c315472543e3
CrazyGuy108 <taylor@hansenfamily.us>
2016-10-04 18:57:04 -0700

    add file extension check
    forgive me for using the C standard library, but it works well without having to get out an entire class to do my bidding

--- src/main.cpp
+++ src/main.cpp
@@ -4,8 +4,15 @@ int main(int argc, char** argv)
 {
 	if (argc == 3)
 	{
-		std::ifstream ifile{ argv[1] };
-		std::ofstream ofile{ argv[2], std::ios::binary };
+		std::ifstream ifile;
+		std::ofstream ofile;
+		const char* iname{ strrchr(argv[1], '.') };
+		const char* oname{ strrchr(argv[2], '.') };
+
+		if (!strcmp(iname, ".asm"))
+			ifile.open(iname);
+		if(!strcmp(oname, ".risc"))
+			ofile.open(oname, std::ios::binary);
 
 		if (ifile.fail())
 			std::cout << "error: input file failed to open\n";
