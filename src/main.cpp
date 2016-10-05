217a8f67d647168468f75d81857296d331afb9e8
CrazyGuy108 <taylor@hansenfamily.us>
2016-10-04 17:59:23 -0700

    temporarily remove file extension check

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,10 @@
 #include "../include/assembler.h"
 
-/***** move some stuff to a global utilities folder *****/
-typedef void(*extension_t)(int argc, char** argv);
-
-Table<extension_t> extensions
-{
-	{ ".asm", &assemble }
-};
-
 int main(int argc, char** argv)
 {
-	// test
-	argc = 3;
-	argv = new char*[3];
-	argv[1] = "test.asm";
 
 	if (argc >= 2)
-		extensions[".asm"](argc, argv);
+		assemble(argc, argv);
 	else
 		; // error: no file
 
