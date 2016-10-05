cb78066d7be1d10e95a802d91248d0d7327efa67
CrazyGuy108 <taylor@hansenfamily.us>
2016-10-04 18:41:30 -0700

    move fstream handling to main()

--- src/main.cpp
+++ src/main.cpp
@@ -2,11 +2,18 @@
 
 int main(int argc, char** argv)
 {
+	if (argc == 3)
+	{
+		std::ifstream ifile{ argv[1] };
+		std::ofstream ofile{ argv[2], std::ios::binary };
 
-	if (argc >= 2)
-		assemble(argc, argv);
-	else
-		; // error: no file
+		if (ifile.fail())
+			std::cout << "error: input file failed to open\n";
+		else if (ofile.fail())
+			std::cout << "error: output file failed to open\n";
+		else
+			assemble(ifile, ofile);
+	}
 
 	return 0;
 }
