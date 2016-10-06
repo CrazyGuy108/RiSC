a3bfd3575d5c149097d2e2f93689126380dafeae
CrazyGuy108 <taylor@hansenfamily.us>
2016-10-05 18:53:07 -0700

    Add file checks and emulator call

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,25 @@
 #include <fstream>  // for ifstream
 #include <iostream> // for cout
+#include "../include/emulator.h"
 
 int main(int argc, char** argv)
 {
+	if (argc == 2)
+	{
+		const char* ext{ strrchr(argv[1], '.') };
+		if (ext && !strcmp(ext, ".risc"))
+		{
+			std::ifstream ifile{ argv[1] };
+			if (ifile.is_open())
+				emulator(ifile);
+			else
+				std::cout << "error: input file failed to open\n";
+		}
+		else
+			std::cout << "error: invalid file extension\n";
+	}
+	else
+		std::cout << "error: input file not specified\n";
+
 	return 0;
 }
