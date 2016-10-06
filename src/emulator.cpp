fc218419c526d77f77789805e41e3b55a91002ef
CrazyGuy108 <taylor@hansenfamily.us>
2016-10-05 19:25:33 -0700

    Move file handling to main()
    The emulator should only be emulating, not handling the input file and all that.

--- src/emulator.cpp
+++ src/emulator.cpp
@@ -2,15 +2,5 @@
 
 void emulator(std::ifstream& ifile)
 {
-	uint16_t* contents;
-	size_t length;
-
-	// get length of file
-	ifile.seekg(0, std::ifstream::end);
-	length = ifile.tellg();
-	ifile.seekg(0, std::ifstream::beg);
-
-	// copy file
-	contents = new uint16_t[length / sizeof(char)];
-	ifile.read((char*)contents, length);
+	
 }
--- src/main.cpp
+++ src/main.cpp
@@ -11,7 +11,19 @@ int main(int argc, char** argv)
 		{
 			std::ifstream ifile{ argv[1] };
 			if (ifile.is_open())
-				emulator(ifile);
+			{
+				uint16_t* contents;
+				size_t length;
+
+				// get length of file
+				ifile.seekg(0, std::ifstream::end);
+				length = ifile.tellg();
+				ifile.seekg(0, std::ifstream::beg);
+
+				// copy file
+				contents = new uint16_t[length / sizeof(char)];
+				ifile.read((char*)contents, length);
+			}
 			else
 				std::cout << "error: input file failed to open\n";
 		}
