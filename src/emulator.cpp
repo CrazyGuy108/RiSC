3377bb0816e747bc762a86fa3b7dec9f829b4ab0
CrazyGuy108 <taylor@hansenfamily.us>
2016-10-07 16:12:49 -0700

    Add mem variable for handling ram
    The RiSC architecture specifies that memory is aligned in 16bit words, not 8bit bytes

--- src/emulator.cpp
+++ src/emulator.cpp
@@ -2,10 +2,18 @@
 
 void emulator(char* contents, size_t length)
 {
+	std::cout << "Initializing memory...\n";
+
+	uint16_t* mem{ new uint16_t[65536] };
+	for (size_t i{ 0 }; i < 65536; ++i)
+		mem[i] = 0;
+
 	uint16_t inst;
 
 	for (size_t i{ 0 }; i < length >> 1; ++i)
 	{
 		inst = reinterpret_cast<uint16_t*>(contents)[i];
 	}
+
+	delete[] mem;
 }
