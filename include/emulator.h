8358cb5a170fa88474e5a7d967ffe410481af403
CrazyGuy108 <taylor@hansenfamily.us>
2016-10-07 15:45:07 -0700

    Add utilities include

--- include/emulator.h
+++ include/emulator.h
@@ -1,8 +1,9 @@
 #ifndef EMULATOR_H
 #define EMULATOR_H
 
-#include <fstream>  // for ifstream
-#include <iostream> // for cout
+#include <fstream>     // for ifstream
+#include <iostream>    // for cout
+#include "utilities.h" // for opcode constants
 
 void emulator(char* contents, size_t length);
 
