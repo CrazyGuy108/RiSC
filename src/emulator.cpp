9027bb250a1b083c692e742613604518fe79d61a
CrazyGuy108 <taylor@hansenfamily.us>
2016-10-07 16:05:46 -0700

    Remove unaligned byte check
    Not needed since rightshifting length by 1 truncates that lsb

--- src/emulator.cpp
+++ src/emulator.cpp
@@ -2,10 +2,6 @@
 
 void emulator(char* contents, size_t length)
 {
-	// if unaligned number of bytes, truncate
-	if (length % 2)
-		length -= 1;
-
 	uint16_t inst;
 
 	for (size_t i{ 0 }; i < length >> 1; ++i)
