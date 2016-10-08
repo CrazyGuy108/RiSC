d75bf95273b27e9216ef25ef58e8f6b4dfd783db
CrazyGuy108 <taylor@hansenfamily.us>
2016-10-08 13:58:21 -0700

    Add commit() member function
    Takes in a Result object and commits it to one of the registers.

--- include/machine.h
+++ include/machine.h
@@ -1,10 +1,14 @@
 #ifndef MACHINE_H
 #define MACHINE_H
 
-#include "utilities.h"
+#include "result.h"    // for Result
+#include "utilities.h" // for typedefs
 
 class Machine
 {
+public:
+	void commit(Result& r);
+
 private:
 	data_t mem[mem_length]  = {};
 	data_t regs[regs_count] = {};
