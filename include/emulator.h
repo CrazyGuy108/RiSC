e5e044f0989b69d0d63b1983fb231e55eaa05d42
CrazyGuy108 <taylor@hansenfamily.us>
2016-10-07 21:23:11 -0700

    Add opcodes include for emulator
    Since it uses a static array of opcode function pointers, it needs to include this part.

--- include/emulator.h
+++ include/emulator.h
@@ -2,6 +2,7 @@
 #define EMULATOR_H
 
 #include <iostream>    // for cout
+#include "opcodes.h"   // for opcode functions
 #include "utilities.h" // for opcode constants
 
 void emulator(char* contents, size_t length);
