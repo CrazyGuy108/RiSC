9d8d88534d30817708c58692a79538e5712dcea9
CrazyGuy108 <taylor@hansenfamily.us>
2016-10-07 17:11:00 -0700

    Create OPCODE macro function
    Used for generating opcode functions with the exact same signature.

--- include/opcodes.h
+++ include/opcodes.h
@@ -4,6 +4,8 @@
 #include "result.h"
 #include "utilities.h"
 
-Result add(inst_t inst, uint16_t* mem);
+#define OPCODE(name) Result name##(inst_t inst, uint16_t* mem)
+
+OPCODE(add);
 
 #endif // OPCODES_H
