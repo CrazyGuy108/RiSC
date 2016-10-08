91c07305341f8d3a4737dc19a57dd6129993b233
CrazyGuy108 <taylor@hansenfamily.us>
2016-10-08 14:47:51 -0700

    Implement commit()
    Uses a switch statement with Result's Dest enum and the length constants to check what to write to.

--- src/machine.cpp
+++ src/machine.cpp
@@ -1,1 +1,21 @@
 #include "../include/machine.h"
+
+void Machine::commit(Result& r)
+{
+	switch (r.dest)
+	{
+	case Result::REGISTERS:
+		if (r.id < regs_count)
+			regs[r.id] = r.value;
+		else
+			; // error: reg index out of range
+		break;
+
+	case Result::MEMORY:
+		if (r.id < mem_length)
+			mem[r.id] = r.value;
+		else
+			; // error: mem index out of range
+		break;
+	}
+}
