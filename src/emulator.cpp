4dcfc77afd3f7951c3154718d93d15c553ca69c6
CrazyGuy108 <taylor@hansenfamily.us>
2016-10-10 16:30:58 -0700

    Remove reminder

--- src/emulator.cpp
+++ src/emulator.cpp
@@ -4,7 +4,7 @@ void emulator(char* contents, size_t length)
 {
 	static OPCODE((*const ops[8])){ &add, &addi, &nand, &lui, &sw, &lw, &beq, &jalr };
 
-	inst_t inst; // change to inst_t
+	inst_t inst;
 
 	for (size_t i{ 0 }; i < length >> 1; ++i)
 	{
