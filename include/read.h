02431e0497ac43ac04be62320e299c876c0f39b3
CrazyGuy108 <taylor@hansenfamily.us>
2016-10-10 12:34:49 -0700

    Add Read class

--- include/read.h
+++ include/read.h
@@ -1,4 +1,13 @@
 #ifndef READ_H
 #define READ_H
 
+#include "memory.h" // for Memory
+
+class Read
+	: public Memory
+{
+public:
+	Read(MemUnit unit, size_t addr);
+};
+
 #endif // READ_H
--- src/read.cpp
+++ src/read.cpp
@@ -1,1 +1,4 @@
 #include "../include/read.h"
+
+Read::Read(MemUnit unit, size_t addr)
+	: Memory{ unit, addr } {}
