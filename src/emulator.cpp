d32a91fe13487e0ceceb6f1b9ec42a8761a69cfe
CrazyGuy108 <taylor@hansenfamily.us>
2016-10-10 16:06:00 -0700

    Use inst_t instead of uint16_t
    Self-documentation purposes.

--- src/emulator.cpp
+++ src/emulator.cpp
@@ -4,11 +4,11 @@ void emulator(char* contents, size_t length)
 {
 	static OPCODE((*const ops[8])){ &add, &addi, &nand, &lui, &sw, &lw, &beq, &jalr };
 
-	uint16_t inst;
+	inst_t inst; // change to inst_t
 
 	for (size_t i{ 0 }; i < length >> 1; ++i)
 	{
-		inst = ((uint16_t*)contents)[i];
+		inst = ((inst_t*)contents)[i];
 		ops[inst & op_mask >> op_shift](inst);
 	}
 }
