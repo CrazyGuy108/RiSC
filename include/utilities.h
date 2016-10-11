538c3cf2ed16a059890b43ddb61ad51bee14dbf5
CrazyGuy108 <taylor@hansenfamily.us>
2016-10-10 17:55:32 -0700

    Add beq constant back
    Forgot to add it a couple commits ago where the opcode constants were added back.

--- include/utilities.h
+++ include/utilities.h
@@ -13,7 +13,8 @@ constexpr inst_t addi = 0x2000;
 constexpr inst_t nand = 0x4000;
 constexpr inst_t lui  = 0x6000;
 constexpr inst_t sw   = 0x8000;
-constexpr inst_t lw   = 0xC000;
+constexpr inst_t lw   = 0xA000;
+constexpr inst_t beq  = 0xC000;
 constexpr inst_t jalr = 0xE000;
 
 // operand masks
