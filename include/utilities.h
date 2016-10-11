0759037858bad8245242744bfec0c66f431d3331
CrazyGuy108 <taylor@hansenfamily.us>
2016-10-10 17:33:36 -0700

    Add back opcode constants
    Since the opcode functions are now contained inside Machine's scope, it's safe to add these constexprs back.

--- include/utilities.h
+++ include/utilities.h
@@ -7,6 +7,15 @@ typedef uint16_t inst_t;  // instruction word
 typedef uint16_t uword_t; // unsigned data word
 typedef int16_t  sword_t; // signed data word
 
+// opcodes
+constexpr inst_t add  = 0x0000;
+constexpr inst_t addi = 0x2000;
+constexpr inst_t nand = 0x4000;
+constexpr inst_t lui  = 0x6000;
+constexpr inst_t sw   = 0x8000;
+constexpr inst_t lw   = 0xC000;
+constexpr inst_t jalr = 0xE000;
+
 // operand masks
 constexpr inst_t op_mask  = 0xE000;
 constexpr inst_t ra_mask  = 0x1C00;
