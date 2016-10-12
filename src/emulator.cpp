f86f24e59089fd9555caff083b89eee448d8dfa5
CrazyGuy108 <taylor@hansenfamily.us>
2016-10-12 16:51:50 -0700

    Use endian-safe loop in emulator()
    The previous method (which involved a pointer cast) would have the 2 bytes either the right way or swapped depending on the endianness of the architecture running this code.

--- src/emulator.cpp
+++ src/emulator.cpp
@@ -4,8 +4,7 @@ void emulator(char* contents, size_t length)
 {
 	RiSC machine;
 
-	for (size_t i{ 0 }; i < length >> 1; ++i)
-	{
-		machine.execute(((inst_t*)contents)[i]);
-	}
+	for (size_t i{ 0 }; i < length; ++i)
+		if (i % 2)
+			machine.execute(((inst_t)contents[i - 1] << 8) | (inst_t)contents[i]);
 }
