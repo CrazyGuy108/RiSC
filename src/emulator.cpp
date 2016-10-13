5c1939853c7d834c49d2314336dd8ce4b0e951e0
CrazyGuy108 <taylor@hansenfamily.us>
2016-10-12 18:42:57 -0700

    Add print register loop
    Used for displaying the results of the given program after execution. This feature may be removed after syscalls are implemented.

--- src/emulator.cpp
+++ src/emulator.cpp
@@ -4,7 +4,12 @@ void emulator(char* contents, size_t length)
 {
 	RiSC machine;
 
+	// execute code
 	for (size_t i{ 0 }; i < length; ++i)
 		if (i % 2)
 			machine.execute(((inst_t)contents[i - 1] << 8) | (inst_t)contents[i]);
+
+	// print register contents
+	for (size_t i{ 0 }; i < regs_count; ++i)
+		std::cout << "r" << i << ": " << machine.getReg(i) << '\n';
 }
