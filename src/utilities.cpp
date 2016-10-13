87e9cd5c49645affb8b72550f5ce4f16e3790424
CrazyGuy108 <taylor@hansenfamily.us>
2016-10-12 17:29:51 -0700

    Implement opcode and register address getters
    Using the constants defined in the header, these functions/getters get the specified operand from an instruction.

--- src/utilities.cpp
+++ src/utilities.cpp
@@ -2,27 +2,27 @@
 
 OPERAND(op)
 {
-
+	return (inst & op_mask) >> op_shift;
 }
 
 OPERAND(ra)
 {
-
+	return (inst & ra_mask) >> ra_shift;
 }
 
 OPERAND(rb)
 {
-
+	return (inst & rb_mask) >> rb_shift;
 }
 
 OPERAND(rc)
 {
-
+	return inst & rc_mask;
 }
 
 OPERAND(i7)
 {
-
+	
 }
 
 OPERAND(i10)
