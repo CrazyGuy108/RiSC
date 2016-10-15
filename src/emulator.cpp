d44b203e8da03df4b3c684052f3e3debc8195e55
CrazyGuy108 <taylor@hansenfamily.us>
2016-10-14 19:21:08 -0700

    Update emulator to use new RiSC functions
    Forgive me for using reinterpret_cast, but it's a temporary endian-specific fix for now.

--- src/emulator.cpp
+++ src/emulator.cpp
@@ -4,8 +4,9 @@ void emulator(char* contents, size_t length)
 {
 	RiSC machine;
 	
-	// execute code
-	machine.execute(0, length);
+	// load and execute bytecode
+	machine.load(reinterpret_cast<data_t*>(contents), length);
+	machine.execute();
 
 	// print register contents
 	for (size_t i{ 1 }; i < regs_count; ++i)
