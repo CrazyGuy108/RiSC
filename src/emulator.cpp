de0910c6c530bda4bbcfc8bad8393667460e6537
CrazyGuy108 <taylor@hansenfamily.us>
2016-10-14 21:09:22 -0700

    Fix over-iteration of code array
    Forgot to divide i by 2 when indexing, because inst_t is 2 bytes while char is only 1.

--- src/emulator.cpp
+++ src/emulator.cpp
@@ -8,7 +8,7 @@ void emulator(char* contents, size_t length)
 	inst_t* code{ new inst_t[length / 2] };
 	for (size_t i{ 0 }; i < length; ++i)
 		if (i % 2)
-			code[i] = contents[i - 1] << 8 | contents[i];
+			code[i / 2] = contents[i - 1] << 8 | contents[i];
 
 	// load the bytecode
 	machine.load(code, length / 2);
