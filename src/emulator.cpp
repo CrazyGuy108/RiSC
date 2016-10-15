1461e14f1ad0db5f629181bec21a0162df6a0d29
CrazyGuy108 <taylor@hansenfamily.us>
2016-10-14 19:59:30 -0700

    Use endian-safe copy loop
    Some architectures have it where arrays are reversed, so an array of 0x01 and 0x00 could be ordered as 0x00,0x01 instead.

--- src/emulator.cpp
+++ src/emulator.cpp
@@ -4,8 +4,14 @@ void emulator(char* contents, size_t length)
 {
 	RiSC machine;
 	
+	// format bytes into 2byte instructions
+	inst_t* code;
+	for (size_t i{ 0 }; i < length; ++i)
+		if (i % 2)
+			code[i] = contents[i - 1] << 8 | contents[i];
+
 	// load and execute bytecode
-	machine.load(reinterpret_cast<data_t*>(contents), length);
+	machine.load(code, length / 2);
 	machine.execute();
 
 	// print register contents
