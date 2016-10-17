fb385aab25ea10f94d8fe591f8cca2a92d41d1ec
CrazyGuy108 <taylor@hansenfamily.us>
2016-10-16 21:59:07 -0700

    Add switch statement to main lexer loop
    Here, a switch statement is setup for the different possibilities of program[i], like for example a space/tab which would create a new token.

--- asm-redo/src/lexer.cpp
+++ asm-redo/src/lexer.cpp
@@ -2,13 +2,21 @@
 
 std::vector<Lexeme> lexer(char* program)
 {
-	std::vector<Lexeme> lexemes;
+	std::vector<Lexeme> lexemes; // keeps track of all the Lexemes the lexer creates
+	char* iterator{ program };   // base pointer for tokens
 
 	// iterate through characters
 	for (size_t i{ 0 }; program[i] != '\0'; ++i)
 	{
-		if (program[i] == ' ')
-			; // tokenize
+		switch (program[i])
+		{
+		case ' ':
+		case '\t':
+			iterator[i] = '\0';                     // terminate new substring,
+			lexemes.push_back(tokenizer(iterator)); // tokenize it,
+			iterator = &iterator[i + 1];            // then reset the base pointer
+			break;
+		}
 	}
 }
 
