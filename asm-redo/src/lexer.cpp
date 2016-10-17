b871923473df1d21be9b7cb19573dbcd6b5c29e2
CrazyGuy108 <taylor@hansenfamily.us>
2016-10-16 22:06:24 -0700

    Add foundSpace boolean for stripping out multiple spaces
    This bool essentially allows the lexer to completely ignore trailing/repeating whitespace without explicitly having to remove it. This is a very important life lesson. Be like the lexer, who ignores what he doesn't like rather than outright attacking it.

--- asm-redo/src/lexer.cpp
+++ asm-redo/src/lexer.cpp
@@ -4,6 +4,7 @@ std::vector<Lexeme> lexer(char* program)
 {
 	std::vector<Lexeme> lexemes; // keeps track of all the Lexemes the lexer creates
 	char* iterator{ program };   // base pointer for tokens
+	bool foundSpace{ true };     // if the lexer has found a space, this will be true
 
 	// iterate through characters
 	for (size_t i{ 0 }; program[i] != '\0'; ++i)
@@ -12,6 +13,9 @@ std::vector<Lexeme> lexer(char* program)
 		{
 		case ' ':
 		case '\t':
+			if (foundSpace) // skips multiple spaces/tabs
+				break;
+
 			iterator[i] = '\0';                     // terminate new substring,
 			lexemes.push_back(tokenizer(iterator)); // tokenize it,
 			iterator = &iterator[i + 1];            // then reset the base pointer
