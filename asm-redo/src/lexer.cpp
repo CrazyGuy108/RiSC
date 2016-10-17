2cd620187db4bbf1de5248572f2c4f0342415b12
CrazyGuy108 <taylor@hansenfamily.us>
2016-10-16 21:44:17 -0700

    Add primary parse loop
    This loop here will be going through every character and checking different things like spaces to form words and other things.

--- asm-redo/src/lexer.cpp
+++ asm-redo/src/lexer.cpp
@@ -2,5 +2,12 @@
 
 std::vector<Lexeme> lexer(char* program)
 {
+	std::vector<Lexeme> lexemes;
 
+	// iterate through characters
+	for (size_t i{ 0 }; program[i] != '\0'; ++i)
+	{
+		if (program[i] == ' ')
+			; // tokenize
+	}
 }
