cd0739c95e1f60d18bfb067d52383d3e55640c6a
CrazyGuy108 <taylor@hansenfamily.us>
2016-10-19 19:44:45 -0700

    Clean up header
    The header looked a little hard to read with several comments in the way of a bunch of function prototypes.

--- asm-redo/inc/lexer.h
+++ asm-redo/inc/lexer.h
@@ -11,26 +11,17 @@ std::vector<Lexeme> lexer(char* program);
 // tokenizes a particular token
 Lexeme tokenizer(char* name);
 
-// checks if the given token is a label and formats it
+// token name checkers
+
 bool isLabelName(char* name);
-
-// checks if the given token name is a register name
-// regex equivalent: ^(r)?[0-7]{1}(,)?\b
 bool isRegName(const char* name);
-
-// checks if the given token is an immediate
 bool isImmName(const char* name);
 
-// resolves a label name into either a local label or a regular one
+// token name formatters
+
 Lexeme::Category label(const char* name);
-
-// resolves and formats a register name
 Lexeme::Category reg(char* name);
-
-// resolves an immediate name
 Lexeme::Category imm(const char* name);
-
-// resolves a token into either a reserved word/opcode or an identifier
 Lexeme::Category resolve(const char* name);
 
 #endif // LEXER_H
