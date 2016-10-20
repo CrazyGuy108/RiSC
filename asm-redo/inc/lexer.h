c25eebc37950afde2a353561772dd8e92bc53d65
CrazyGuy108 <taylor@hansenfamily.us>
2016-10-19 21:26:43 -0700

    Add static token parsing member functions to Lexer class
    These functions will be identical to the ones currently in use. The reason why they're still currently in use is so the code is still testable for now.

--- asm-redo/inc/lexer.h
+++ asm-redo/inc/lexer.h
@@ -32,8 +32,21 @@ class Lexer
 {
 public:
 	Lexeme next(); // tokenizes the next lexeme
-
+	
 private:
+	// token name checkers
+
+	static TOKEN_CHECK(isLabelName);
+	static TOKEN_CHECK(isRegName);
+	static TOKEN_CHECK(isImmName);
+
+	// token name formatters
+
+	static TOKEN_FORMAT(formatLabel);
+	static TOKEN_FORMAT(formatReg);
+	static TOKEN_FORMAT(formatImm);
+	static TOKEN_FORMAT(formatKeyword);
+
 	char* iterator; // pointer that iterates through the program
 	size_t index;   // index of the character relative to iterator currently being analyzed
 };
