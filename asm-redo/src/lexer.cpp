fcdb00c4bf4db3ac26cf7fcfacaa0d7bc9254c51
CrazyGuy108 <taylor@hansenfamily.us>
2016-10-19 17:18:40 -0700

    Rearrange formatting ifs in isRegName()

--- asm-redo/src/lexer.cpp
+++ asm-redo/src/lexer.cpp
@@ -109,15 +109,15 @@ bool isRegName(char* name)
 	      name[1] <= '7' &&
 	      name[2] == ',')))   // r+digit+comma end
 	{
-		// remove optional r
-		if (name[0] == 'r')
-			name = &name[1];
-		
 		// remove optional comma
-		if(len == 2 && name[1] == ',')
+		if (len == 2 && name[1] == ',')
 			name[1] = '\0';
 		else if (len == 3 && name[2] == ',')
 			name[2] = '\0';
+		
+		// remove optional r
+		if (name[0] == 'r')
+			name = &name[1];
 		
 		return true;
 	}
