081b0ffb47ef1a1822ba528fff8307a4a86edd6d
CrazyGuy108 <taylor@hansenfamily.us>
2016-10-19 17:40:34 -0700

    Implement Lexeme getters

--- asm-redo/src/lexeme.cpp
+++ asm-redo/src/lexeme.cpp
@@ -2,3 +2,13 @@
 
 Lexeme::Lexeme(char* name, Category category)
 	: name{ name }, category{ category } {}
+
+const char* Lexeme::getName()
+{
+	return name;
+}
+
+Lexeme::Category Lexeme::getCategory()
+{
+	return category;
+}
