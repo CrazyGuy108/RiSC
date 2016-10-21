5e7adedb9dbee4b8041c429119214b1751d64c2b
CrazyGuy108 <taylor@hansenfamily.us>
2016-10-21 15:43:25 -0700

    Organize Category enum
    Added comments and another line break in order to split the words of the enum into groups to make it more readable.

--- asm-redo/inc/lexeme.h
+++ asm-redo/inc/lexeme.h
@@ -7,8 +7,9 @@ class Lexeme
 public:
 	enum Category
 	{
-		BEGIN, END, IDENTIFIER, LABEL, LOCAL_LABEL, REGISTER, IMMEDIATE, NEWLINE,
-		ADD, ADDI, NAND, LUI, SW, LW, BEQ, JALR
+		BEGIN, END, NEWLINE, // signals to the parser
+		LABEL, LOCAL_LABEL, REGISTER, IMMEDIATE, IDENTIFIER, // types of tokens
+		ADD, ADDI, NAND, LUI, SW, LW, BEQ, JALR // opcodes
 	};
 
 	Lexeme(char* name, Category category);
