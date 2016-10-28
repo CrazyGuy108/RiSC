e2269894a1284fbe0ad8fbed476c591d3745dbec
CrazyGuy108 <taylor@hansenfamily.us>
2016-10-28 16:34:11 -0700

    Add overloads taking a Lexer reference
    Overloading the constructor like this allows a couple different ways to intialize a Parser object. You can give it the program to immediately parse, or give it a Lexer reference to skip a step instead.

--- asm-redo/inc/parser.h
+++ asm-redo/inc/parser.h
@@ -1,13 +1,17 @@
 #ifndef PARSER_H
 #define PARSER_H
 
+#include "lexer.h" // for Lexer
+
 class Parser
 {
 public:
 	Parser() = default;
-	Parser(char* program); // lex and parse program
+	Parser(char* program); // lex and parse the given program
+	Parser(Lexer& lexer);  // parse program using the given lexer
 
-	void parse(char* program);
+	void parse(char* program); // lex and parse the given program
+	void parse(Lexer& lexer);  // parse program using the given lexer
 };
 
 #endif // PARSER_H
--- asm-redo/src/parser.cpp
+++ asm-redo/src/parser.cpp
@@ -5,7 +5,18 @@ Parser::Parser(char* program)
 	parse(program);
 }
 
+Parser::Parser(Lexer& lexer)
+{
+	parse(lexer);
+}
+
 void Parser::parse(char* program)
+{
+	Lexer lexer{ program };
+	parse(lexer);
+}
+
+void Parser::parse(Lexer& lexer)
 {
 
 }
