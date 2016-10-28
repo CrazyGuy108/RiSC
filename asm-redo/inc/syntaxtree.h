b5b132b375553a71680e50d360aa2582e972eae3
CrazyGuy108 <taylor@hansenfamily.us>
2016-10-28 15:45:44 -0700

    Add comments

--- asm-redo/inc/syntaxtree.h
+++ asm-redo/inc/syntaxtree.h
@@ -1,14 +1,14 @@
 #ifndef SYNTAXTREE_H
 #define SYNTAXTREE_H
 
-#include "token.h"
+#include "token.h" // for Token
 
 class SyntaxTree
 {
 public:
 	SyntaxTree() = default;
 
-	void add(Token t);
+	void add(Token t); // adds a Token to the syntax tree
 
 private:
 	// syntax tree stuff
