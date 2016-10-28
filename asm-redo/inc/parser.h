cb79614e378885ce2563d824320a0039ec19b5b1
CrazyGuy108 <taylor@hansenfamily.us>
2016-10-28 16:06:37 -0700

    Add Parser class
    This class will be acting as a container for SyntaxTree and will depend on Lexer in order to build the syntax tree.

--- asm-redo/inc/parser.h
+++ asm-redo/inc/parser.h
@@ -1,4 +1,12 @@
 #ifndef PARSER_H
 #define PARSER_H
 
+class Parser
+{
+public:
+	Parser() = default;
+
+	void parse(char* program);
+};
+
 #endif // PARSER_H
--- asm-redo/src/parser.cpp
+++ asm-redo/src/parser.cpp
@@ -1,1 +1,6 @@
 #include "../inc/parser.h"
+
+void Parser::parse(char* program)
+{
+
+}
