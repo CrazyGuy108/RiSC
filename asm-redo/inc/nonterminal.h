ed317ec679b686f4db3572a3f1f446a7b7f3bf92
CrazyGuy108 <taylor@hansenfamily.us>
2016-10-28 19:55:42 -0700

    Add NonTerminal classes

--- asm-redo/inc/nonterminal.h
+++ asm-redo/inc/nonterminal.h
@@ -1,4 +1,21 @@
 #ifndef NONTERMINAL_H
 #define NONTERMINAL_H
 
+class NonTerminal
+{
+
+};
+
+class Program
+	: public NonTerminal
+{
+
+};
+
+class Line
+	: public NonTerminal
+{
+
+};
+
 #endif // NONTERMINAL_H
