50f3696980fb107f492d85ddddd436e8edf8af7e
CrazyGuy108 <taylor@hansenfamily.us>
2016-10-28 17:28:13 -0700

    Add Terminal class and subclasses.

--- asm-redo/inc/terminal.h
+++ asm-redo/inc/terminal.h
@@ -1,4 +1,33 @@
 #ifndef TERMINAL_H
 #define TERMINAL_H
 
+class Terminal
+{
+
+};
+
+class Label
+	: public Terminal
+{
+
+};
+
+class Opcode
+	: public Terminal
+{
+
+};
+
+class Reg
+	: public Terminal
+{
+
+};
+
+class Imm
+	: public Terminal
+{
+
+};
+
 #endif // TERMINAL_H
