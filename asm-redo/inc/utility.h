a4a836c40433e39d98d3bfa69a46777e39337d18
CrazyGuy108 <taylor@hansenfamily.us>
2016-10-30 09:10:28 -0700

    Add keywords map
    This map allows access to the keywords that the program can accept. They are identified as just a keyword by the lexer and then identifies the type of keyword  by the parser, finally being checked by semantic analysis.

--- asm-redo/inc/utility.h
+++ asm-redo/inc/utility.h
@@ -1,4 +1,19 @@
 #ifndef UTILITY_H
 #define UTILITY_H
 
+#include <map>
+
+enum class Keyword
+{
+	ADD, ADDI, NAND, LUI, SW, LW, BEQ, JALR // opcode
+};
+
+class StrLess
+{
+public:
+	bool operator()(const char* a, const char* b) const;
+};
+
+extern const std::map<const char*, Keyword, StrLess> keywords;
+
 #endif // UTILITY_H
--- asm-redo/src/utility.cpp
+++ asm-redo/src/utility.cpp
@@ -1,1 +1,18 @@
 #include "../inc/utility.h"
+
+bool StrLess::operator()(const char* a, const char* b) const
+{
+	return strcmp(a, b) < 0;
+}
+
+const std::map<const char*, Keyword, StrLess> keywords
+{
+	{ "add",  Keyword::ADD },
+	{ "addi", Keyword::ADDI },
+	{ "nand", Keyword::NAND },
+	{ "lui",  Keyword::LUI },
+	{ "sw",   Keyword::SW },
+	{ "lw",   Keyword::LW },
+	{ "beq",  Keyword::BEQ },
+	{ "jalr", Keyword::JALR }
+};
