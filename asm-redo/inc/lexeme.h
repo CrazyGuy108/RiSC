19ed8b6a928879d0c3619448ab2bcce33e0b54ac
CrazyGuy108 <taylor@hansenfamily.us>
2016-10-30 16:42:48 -0700

    Add class
    The members of the Lexeme class are beg and end, which are the beginning and end of the substring. A basic constructor along with getters are provided for now.

--- asm-redo/inc/lexeme.h
+++ asm-redo/inc/lexeme.h
@@ -1,4 +1,17 @@
 #ifndef LEXEME_H
 #define LEXEME_H
 
+// non-terminated substring of the source code
+class Lexeme
+{
+	Lexeme(const char* beg, const char* end);
+
+	const char* getBeg(); // gets beg
+	const char* getEnd(); // gets end
+
+private:
+	const char* beg; // beginning of lexeme
+	const char* end; // end of lexeme
+};
+
 #endif // LEXEME_H
--- asm-redo/src/lexeme.cpp
+++ asm-redo/src/lexeme.cpp
@@ -1,1 +1,14 @@
 #include "../inc/lexeme.h"
+
+Lexeme::Lexeme(const char* beg, const char* end)
+	: beg{ beg }, end{ end } {}
+
+const char* Lexeme::getBeg()
+{
+	return beg;
+}
+
+const char* Lexeme::getEnd()
+{
+	return end;
+}
