38ee62cf7b3a227c0e785fb0593af5a923134dd4
CrazyGuy108 <taylor@hansenfamily.us>
2016-10-30 09:13:08 -0700

    Add comments

--- asm-redo/inc/utility.h
+++ asm-redo/inc/utility.h
@@ -8,12 +8,14 @@ enum class Keyword
 	ADD, ADDI, NAND, LUI, SW, LW, BEQ, JALR // opcode
 };
 
+// function class for maps containing strings
 class StrLess
 {
 public:
 	bool operator()(const char* a, const char* b) const;
 };
 
+// keywords mapped to the Keyword enum class
 extern const std::map<const char*, Keyword, StrLess> keywords;
 
 #endif // UTILITY_H
