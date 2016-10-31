3fb23da52edffc41efc3ee7a4329f0cb0af752ce
CrazyGuy108 <taylor@hansenfamily.us>
2016-10-31 16:54:14 -0700

    Make keywords map contain a Lexeme instead of a string
    Still making the major shift towards using the Lexeme class instead of a char array.

--- asm-redo/inc/utility.h
+++ asm-redo/inc/utility.h
@@ -1,21 +1,15 @@
 #ifndef UTILITY_H
 #define UTILITY_H
 
-#include <map>
+#include <map>      // for map
+#include "lexeme.h" // for Lexeme
 
 enum class Keyword
 {
 	ADD, ADDI, NAND, LUI, SW, LW, BEQ, JALR // opcode
 };
 
-// function class for maps containing strings
-class StrLess
-{
-public:
-	bool operator()(const char* a, const char* b) const;
-};
-
-typedef std::map<const char*, Keyword, StrLess> keyword_map;
+typedef std::map<Lexeme, Keyword> keyword_map;
 
 // keywords mapped to the Keyword enum class
 extern const keyword_map keywords;
