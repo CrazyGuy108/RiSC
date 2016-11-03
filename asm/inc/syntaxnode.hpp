fd35f6339c642bc1e7e2af99a990b0b2c7c8dd3b
CrazyGuy108 <taylor@hansenfamily.us>
2016-11-03 07:46:28 -0700

    Add Terminal and NonTerminal classes
    These are two polymorphic classes that will be representing symbols in the grammar.

--- asm/inc/syntaxnode.hpp
+++ asm/inc/syntaxnode.hpp
@@ -1,4 +1,22 @@
 #ifndef SYNTAXNODE_HPP
 #define SYNTAXNODE_HPP
 
+// base class for terminal and nonterminal symbols
+class SyntaxNode
+{
+
+};
+
+class Terminal
+	: public SyntaxNode
+{
+
+};
+
+class NonTerminal
+	: public SyntaxNode
+{
+
+};
+
 #endif // SYNTAXNODE_HPP
