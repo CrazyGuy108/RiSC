b77606ad68e42cf4b770ed49d0e2421d5afbc563
CrazyGuy108 <taylor@hansenfamily.us>
2016-11-05 13:40:25 -0700

    Replace Production class with typedef
    It would be useless to have another class that contains a vector without any added functionality, so I turned it into a typedef instead.

--- asm/inc/parsetable.hpp
+++ asm/inc/parsetable.hpp
@@ -1,12 +1,12 @@
 #ifndef PARSETABLE_HPP
 #define PARSETABLE_HPP
 
+#include <vector> // for vector
+#include "parsenode.hpp" // for ParseNode
+
+typedef std::vector<ParseNode::Symbol> production_t;
+
 class ParseTable
-{
-
-};
-
-class Production
 {
 
 };
