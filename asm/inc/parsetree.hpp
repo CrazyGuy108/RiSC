af39b1f247e708fec0445901b9731f834f44dca1
CrazyGuy108 <taylor@hansenfamily.us>
2016-11-04 21:48:55 -0700

    Delete copy constructor and assignment operator
    Since this is a polymorphic tree, copying pointers will have consequences like deleting what has already been deleted.

--- asm/inc/parsetree.hpp
+++ asm/inc/parsetree.hpp
@@ -11,6 +11,9 @@ public:
 	ParseTree(const char* program); // lex and parse the given program
 	ParseTree(Lexer& lexer);        // parse the program using the given lexer
 
+	ParseTree(const ParseTree&) = delete;
+	ParseTree& operator=(const ParseTree&) = delete;
+
 	void parse(const char* program); // lex and parse the given program
 	void parse(Lexer& lexer);        // parse the program using the given lexer
 
