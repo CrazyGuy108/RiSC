efc6246d32b177eaea4ef1c4c0a7c4d7ee14e5a1
CrazyGuy108 <taylor@hansenfamily.us>
2016-11-05 17:16:41 -0700

    Switch to range-based for loop

--- asm/src/parsenode.cpp
+++ asm/src/parsenode.cpp
@@ -62,12 +62,12 @@ NonTerminal::~NonTerminal()
 void NonTerminal::expand(const production_t& p)
 {
 	children.clear();
-	for (size_t i{ 0 }; i < p.size(); ++i)
+	for (ParseNode::Symbol s : p)
 	{
-		if (p[i] >= BEGIN && p[i] <= ERROR) // Terminal
-			children.push_back(new Terminal{ p[i] });
-		else if (p[i] >= program && p[i] <= ri1) // NonTerminal
-			children.push_back(new NonTerminal{ p[i] });
+		if (s >= BEGIN && s <= ERROR) // Terminal
+			children.push_back(new Terminal{ s });
+		else if (s >= program && s <= ri1) // NonTerminal
+			children.push_back(new NonTerminal{ s });
 		else
 			; // error!
 	}
