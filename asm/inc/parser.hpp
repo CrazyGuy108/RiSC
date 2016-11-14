45198476456f0c062f50e2f53d63c1b2a578aadf
CrazyGuy108 <taylor@hansenfamily.us>
2016-11-13 21:08:07 -0800

    Add main Parser function

--- asm/inc/parser.hpp
+++ asm/inc/parser.hpp
@@ -1,10 +1,19 @@
 #ifndef PARSER_HPP
 #define PARSER_HPP
 
+#include "lexer.hpp" // for Lexer
+
 // verifies and organizes a stream of tokens from the Lexer
 class Parser
 {
+public:
+	Parser() = default;
+	Parser(Lexer& lexer);
 
+	void parse(Lexer& lexer); // parse using the given lexer
+
+private:
+	// stream of lines
 };
 
 #endif // PARSER_HPP
--- asm/src/parser.cpp
+++ asm/src/parser.cpp
@@ -1,1 +1,11 @@
 #include "../inc/parser.hpp"
+
+Parser::Parser(Lexer& lexer)
+{
+	parse(lexer);
+}
+
+void Parser::parse(Lexer& lexer)
+{
+	// parsing stuff
+}
