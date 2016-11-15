fc8ad5ac4a63a8a7073d7d0a94257f2601bdb824
CrazyGuy108 <taylor@hansenfamily.us>
2016-11-14 16:56:14 -0800

    Fill out Line and LineWithLabel classes
    Pretty straightforward, LineWithLabel inherits from Line, allowing one to contain a label when used polymorphically, then just filling out members variables, constructors, and virtual destructor. The operands member isn't added just yet since it'll be its own class.

--- asm/inc/parser.hpp
+++ asm/inc/parser.hpp
@@ -1,7 +1,9 @@
 #ifndef PARSER_HPP
 #define PARSER_HPP
 
-#include "lexer.hpp" // for Lexer
+#include "lexeme.hpp"  // for Lexeme
+#include "lexer.hpp"   // for Lexer
+#include "utility.hpp" // for Keyword
 
 // verifies and organizes a stream of tokens from the Lexer
 class Parser
@@ -20,16 +22,24 @@ private:
 class Line
 {
 public:
-	// opcode
+	Line(Keyword opcode);
+	virtual ~Line();
+
+protected:
+	Keyword opcode;
 	// operands
 };
 
 // similar to a Line but with a label
-class LineWithLabel
+class LineWithLabel final
 	: public Line
 {
 public:
-	// label
+	LineWithLabel(Lexeme label, Keyword opcode);
+	virtual ~LineWithLabel() override final;
+
+private:
+	Lexeme label;
 };
 
 #endif // PARSER_HPP
