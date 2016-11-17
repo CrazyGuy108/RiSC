4f62c158d871267d4f8366a6b1f2526c410c50f5
CrazyGuy108 <taylor@hansenfamily.us>
2016-11-16 19:12:46 -0800

    Add SymbolTable class
    This class is meant to replace the keyword_map typedef. It's used as a table for Lexemes, mapping them onto their corresponding value using std::map.

--- asm/inc/symboltable.hpp
+++ asm/inc/symboltable.hpp
@@ -1,4 +1,20 @@
 #ifndef SYMBOLTABLE_HPP
 #define SYMBOLTABLE_HPP
 
+#include <map>         // for map
+#include "lexeme.hpp"  // for Lexeme
+#include "utility.hpp" // for inst_t
+
+// container adaptor for mapping Lexemes onto inst_t's
+class SymbolTable final
+{
+public:
+	typedef std::map<Lexeme, inst_t> map_type;
+
+	SymbolTable(std::initializer_list<map_type::value_type> il);
+
+private:
+	map_type symbols;
+};
+
 #endif // SYMBOLTABLE_HPP
--- asm/src/symboltable.cpp
+++ asm/src/symboltable.cpp
@@ -1,1 +1,4 @@
 #include "../inc/symboltable.hpp"
+
+SymbolTable::SymbolTable(std::initializer_list<map_type::value_type> il)
+	: symbols{ il } {}
