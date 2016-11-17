edb851195a55b99f9eafeeecfb574011466ea358
CrazyGuy108 <taylor@hansenfamily.us>
2016-11-16 17:09:15 -0800

    Add parser test
    I essentially copypasted the Lexer test code into here. The only thing that needs fixing is the getErrors() function which the Parser class doesn't have yet.

--- asm/src/main.cpp
+++ asm/src/main.cpp
@@ -1,10 +1,12 @@
-#include <fstream>          // for ifstream
-#include <iostream>         // for cout
-#include <string>           // for string
-#include "../inc/lexer.hpp" // for lexer
+#include <fstream>           // for ifstream
+#include <iostream>          // for cout
+#include <string>            // for string
+#include "../inc/lexer.hpp"  // for Lexer
+#include "../inc/parser.hpp" // for Parser
 
 #define READ_TEST
 #define LEXER_TEST
+#define PARSER_TEST
 
 int main()
 {
@@ -26,10 +28,19 @@ int main()
 		while (!lexer.empty())
 			std::cout << lexer.next() << '\n';
 
-		std::cout << "errors found: " << lexer.getErrors() << '\n';
-
-		std::cout << std::endl;
+		std::cout << "errors found: " << lexer.getErrors() << '\n' << std::endl;
 #endif // LEXER_TEST
+
+#ifdef PARSER_TEST
+		std::cout << "Parser:" << std::endl;
+		Parser parser{ Lexer{ contents.c_str() } };
+
+		// print out every Line created by the parser
+		while(!parser.empty())
+			std::cout << *parser.next() << '\n';
+
+		std::cout << "errors found: " << parser.getErrors() << '\n' << std::endl;
+#endif // PARSER_TEST
 
 		ifile.close();
 		std::cout << "input file closed\n";
