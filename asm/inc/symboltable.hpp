9fd2b0db8956294a0e6b362fdb40708eb8943a4f
CrazyGuy108 <taylor@hansenfamily.us>
2016-11-16 20:53:04 -0800

    Add iterator typedefs

--- asm/inc/symboltable.hpp
+++ asm/inc/symboltable.hpp
@@ -9,8 +9,12 @@
 class SymbolTable final
 {
 public:
-	typedef std::map<Lexeme, inst_t> map_type;
-	typedef map_type::value_type     value_type;
+	typedef std::map<Lexeme, inst_t>         map_type;
+	typedef map_type::value_type             value_type;
+	typedef map_type::iterator               iterator;
+	typedef map_type::reverse_iterator       reverse_iterator;
+	typedef map_type::const_iterator         const_iterator;
+	typedef map_type::const_reverse_iterator const_reverse_iterator;
 
 	SymbolTable() = default;
 	SymbolTable(std::initializer_list<value_type> il);
