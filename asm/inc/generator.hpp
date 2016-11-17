4e90430e5c05aaa946565078b91b3b1d5d178102
CrazyGuy108 <taylor@hansenfamily.us>
2016-11-17 07:45:25 -0800

    Add class

--- asm/inc/generator.hpp
+++ asm/inc/generator.hpp
@@ -1,4 +1,24 @@
 #ifndef GENERATOR_HPP
 #define GENERATOR_HPP
 
+#include <queue>       // for queue
+#include "parser.hpp"  // for Parser
+#include "utility.hpp" // for inst_t
+
+class Generator
+{
+public:
+	Generator() = default;
+	Generator(Parser& parser);
+
+	void generate(Parser& parser);
+	inst_t next();
+	bool empty() const;
+	size_t getErrors() const;
+
+private:
+	std::queue<inst_t> bytecode;
+	size_t errors;
+};
+
 #endif // GENERATOR_HPP
