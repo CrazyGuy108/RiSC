c05cff04325364f1fba2c087eb1692ef7d26bfc4
CrazyGuy108 <taylor@hansenfamily.us>
2016-11-17 17:46:05 -0800

    Implement Generator functions
    The generate() function is left blank for now because that's where the main part will be going.

--- asm/src/generator.cpp
+++ asm/src/generator.cpp
@@ -1,1 +1,34 @@
 #include "../inc/generator.hpp"
+
+Generator::Generator(Parser& parser)
+	: errors{ 0 }
+{
+	generate(parser);
+}
+
+void Generator::generate(Parser& parser)
+{
+
+}
+
+inst_t Generator::next()
+{
+	if (!empty())
+	{
+		inst_t tmp{ bytecode.front() };
+		bytecode.pop();
+		return tmp;
+	}
+	else
+		return 0; // create a nop
+}
+
+bool Generator::empty() const
+{
+	return bytecode.empty();
+}
+
+size_t Generator::getErrors() const
+{
+	return errors;
+}
