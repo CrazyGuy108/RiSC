fa181d4b9c280efbde0015423f1a47f8f85e295e
CrazyGuy108 <taylor@hansenfamily.us>
2016-11-17 19:47:28 -0800

    Add enum class Bitwise to Generator
    This enum is used by the compile() function to assemble a line of bytecode.

--- asm/inc/generator.hpp
+++ asm/inc/generator.hpp
@@ -22,12 +22,34 @@ public:
 	const symbol_table& getSymbolTable() const;
 
 private:
+	enum class Bitwise : inst_t;
+
 	void compile(Line* line); // uses symtable to generate a line of code
 
 	bytecode_list bytecode;           // stream of bytecode
 	bytecode_list::const_iterator it; // keeps track of position
 	size_t errors;                    // amount of errors encountered
 	symbol_table symtable;            // table for resolving labels
+};
+
+enum class Generator::Bitwise : inst_t
+{
+	RA_SHIFT = 10,
+	RB_SHIFT = 7,
+	RA_MASK  = 0x1C00,
+	RB_MASK  = 0x0380,
+	RC_MASK  = 0x0007,
+	I7_MASK  = 0x007F,
+	I10_MASK = 0x03FF,
+
+	ADD  = 0x0000,
+	ADDI = 0x2000,
+	NAND = 0x4000,
+	LUI  = 0x6000,
+	SW   = 0x8000,
+	LW   = 0xA000,
+	BEQ  = 0xC000,
+	JALR = 0xE000
 };
 
 #endif // GENERATOR_HPP
