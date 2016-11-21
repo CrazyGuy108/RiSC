46cb67b65a97d92b00afa11f420d46ab620b87b0
CrazyGuy108 <taylor@hansenfamily.us>
2016-11-20 22:11:22 -0800

    remove the need for a utilities header
    The only dependency on this header is the RiSC class, so I sought to
    remove that dependency.
    
    A Bitwise enum was added as a private member to replace the long line of
    constexpr inst_t's.
    
    The operand getters were inserted as private static helpers.
    
    Typedefs were moved inside the class as public, usable typedefs.
    
    The 2 length constants regs_count and ram_length were inlined instead.
    
    The Memory template using declaration was also moved inside the class.

--- emu/inc/risc.hpp
+++ emu/inc/risc.hpp
@@ -2,28 +2,71 @@
 #define RISC_HPP
 
 #include <array>         // for array
+#include <cstdint>       // for uint16_t
 #include <iostream>      // for cout
-#include "utilities.hpp" // for typedefs
-
-template<size_t len>
-using Memory = std::array<data_t, len>;
 
 class RiSC
 {
 public:
+	template<size_t len>
+	using Memory = std::array<data_t, len>;
+
+	typedef uint16_t inst_t; // instruction word
+	typedef uint16_t data_t; // data word
+
 	RiSC();
 
 	void load(const char* program, data_t length); // load a program into memory
 	void execute(); // execute the program where the PC left off
 
 private:
+	enum Bitwise
+		: inst_t;
+
 	data_t& reg(data_t addr);
-	data_t reg(data_t addr) const;
+	data_t  reg(data_t addr) const;
 
-	Memory<ram_length> ram;
-	Memory<regs_count> regs;
+	static inst_t op(inst_t inst);
+	static inst_t ra(inst_t inst);
+	static inst_t rb(inst_t inst);
+	static inst_t rc(inst_t inst);
+	static inst_t i7(inst_t inst);
+	static inst_t i10(inst_t inst);
 
-	data_t pc;
+	Memory<65536> ram;
+	Memory<8>     regs;
+	data_t        pc;
+};
+
+enum RiSC::Bitwise
+	: RiSC::inst_t
+{
+	// opcodes
+
+	ADD  = 0x0000,
+	ADDI = 0x2000,
+	NAND = 0x4000,
+	LUI  = 0x6000,
+	SW   = 0x8000,
+	LW   = 0xA000,
+	BEQ  = 0xC000,
+	JALR = 0xE000,
+
+	// bitmasks
+
+	OP_MASK  = 0xE000,
+	RA_MASK  = 0x1C00,
+	RB_MASK  = 0x0380,
+	RC_MASK  = 0x0007,
+	I7_MASK  = 0x003F,
+	I7_SIGN  = 0x0020,
+	I10_MASK = 0x03FF,
+
+	// bitshifts
+
+	RA_SHIFT  = 10,
+	RB_SHIFT  = 7,
+	I10_SHIFT = 6
 };
 
 #endif // RISC_HPP
--- emu/src/risc.cpp
+++ emu/src/risc.cpp
@@ -17,36 +17,36 @@ void RiSC::execute()
 		inst = ram[pc];
 		switch (op(inst))
 		{
-		case add:
+		case ADD:
 			reg(ra(inst)) = reg(rb(inst)) + reg(rc(inst));
 			break;
 
-		case addi:
+		case ADDI:
 			reg(ra(inst)) = reg(rb(inst)) + i7(inst);
 			break;
 
-		case nand:
+		case NAND:
 			reg(ra(inst)) = ~(reg(rb(inst)) & reg(rc(inst)));
 			break;
 
-		case lui:
+		case LUI:
 			reg(ra(inst)) = i10(inst);
 			break;
 
-		case sw:
+		case SW:
 			ram[reg(rb(inst)) + i7(inst)] = reg(ra(inst));
 			break;
 
-		case lw:
+		case LW:
 			reg(ra(inst)) = ram[reg(rb(inst)) + i7(inst)];
 			break;
 
-		case beq:
+		case BEQ:
 			if (reg(ra(inst)) == reg(rb(inst)))
 				pc += i7(inst);
 			break;
 
-		case jalr:
+		case JALR:
 			if (!i7(inst))
 			{
 				reg(ra(inst)) = pc + 1;
@@ -77,17 +77,48 @@ void RiSC::execute()
 	}
 }
 
-data_t& RiSC::reg(data_t addr)
+RiSC::data_t& RiSC::reg(data_t addr)
 {
 	if(addr == 0)
 		regs[addr] = 0;
 	return regs[addr];
 }
 
-data_t RiSC::reg(data_t addr) const
+RiSC::data_t RiSC::reg(data_t addr) const
 {
 	if (addr == 0)
 		return 0;
 	else
 		return regs[addr];
 }
+
+RiSC::inst_t RiSC::op(inst_t inst)
+{
+	return inst & OP_MASK;
+}
+
+RiSC::inst_t RiSC::ra(inst_t inst)
+{
+	return (inst & RA_MASK) >> RA_SHIFT;
+}
+
+RiSC::inst_t RiSC::rb(inst_t inst)
+{
+	return (inst & RB_MASK) >> RB_SHIFT;
+}
+
+RiSC::inst_t RiSC::rc(inst_t inst)
+{
+	return inst & RC_MASK;
+}
+
+RiSC::inst_t RiSC::i7(inst_t inst)
+{
+	return inst & I7_MASK | (inst & I7_SIGN ? ~I7_MASK : 0);
+}
+
+RiSC::inst_t RiSC::i10(inst_t inst)
+{
+	return (inst & I10_MASK) << I10_SHIFT;
+}
+
